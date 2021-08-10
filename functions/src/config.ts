30ae0c9241e7a1240d5ad4aed641224fb0e8c4db
Joe Carlton <joe@rconjoe.com>
2021-08-10 15:21:32 -0400

    instantiate stripe

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,7 +1,16 @@
+import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 admin.initializeApp()
+
+import Stripe from 'stripe'
 
 export const db = admin.firestore()
 const settings = { timestampsInSnapshots: true }
 db.settings(settings)
+
+export const stripeSecret = functions.config().stripe.test_key
+
+export const stripe = new Stripe(stripeSecret, {
+  apiVersion: '2020-08-27'
+})
 
