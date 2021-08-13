545c8971973929c7592600d0f9e3748671df26b9
Joe Carlton <joe@rconjoe.com>
2021-08-12 23:49:05 -0400

    properly handle auth instantiation

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,14 +1,14 @@
-import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 admin.initializeApp()
 
 import Stripe from 'stripe'
 
+export const auth = admin.auth()
 export const db = admin.firestore()
 const settings = { timestampsInSnapshots: true }
 db.settings(settings)
 
-export const stripeSecret = functions.config().stripe.test_key
+export const stripeSecret = "sk_test_51HJUgfGoIl5NLNcQ8xzPwo3tXqwoaGym8ZXwPBxbVWuOEEdCQxst4ORTV9x8GU4k4TK9uyFFiB9zLHMvDMLSV9UW00N8C4ejVB"
 
 export const stripe = new Stripe(stripeSecret, {
   apiVersion: '2020-08-27'
