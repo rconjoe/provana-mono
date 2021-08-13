57742104466bf9fa5d1547820232a0499e59e548
Joe Carlton <joe@rconjoe.com>
2021-08-12 23:49:14 -0400

    export registerSupporter

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,12 +2,12 @@ import * as functions from "firebase-functions";
 import { db } from './config'
 
 export const testFunc = functions.https.onCall(async (data, context) => {
-
   const uid = context.auth && context.auth.uid
   const message = data.message
   const write: FirebaseFirestore.DocumentReference =
     await db.collection('test-messages').add({message: message})
 
   return `${uid} said ${message}, and I wrote it to firestore at messages/${write.id}`
-
 })
+
+export { registerSupporter } from './endpoint/registerSupporter'
