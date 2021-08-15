42ea1c2de4a25d9ec410d2bbfdde93b5541541cf
Joe Carlton <joe@rconjoe.com>
2021-08-15 19:08:15 -0400

    add generateAccountLink method to StripeAccountService()

--- functions/src/services/stripe/StripeAccountService.ts
+++ functions/src/services/stripe/StripeAccountService.ts
@@ -1,5 +1,6 @@
 import { Creator } from '../../models/Creator'
 import { stripe } from '../../config'
+import Stripe from 'stripe'
 
 export class StripeAccountService {
   public async create(_newAccount: Creator): Promise<Creator> {
@@ -18,5 +19,14 @@ export class StripeAccountService {
     })
     _newAccount.account = account.id
     return _newAccount
+  }
+
+  public async generateAccountLink(accountId: string): Promise<Stripe.Response<Stripe.AccountLink>> {
+    return await stripe.accountLinks.create({
+      account: accountId,
+      refresh_url: 'http://localhost:8080/onboardRetry',
+      return_url: 'http://localhost:8080/onboardSuccess',
+      type: 'account_onboarding'
+    })
   }
 }
