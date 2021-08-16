cffece208d61341552e67902e1e37a03f073b509
Michael Coen <michaelkimcoen@gmail.com>
2021-08-16 15:07:49 -0400

    created UserAuth class that takes a uid and idToken with the option to set a csrf token

---
+++ functions/src/models/UserAuth.ts
@@ -0,0 +1,75 @@
+/**
+ * @fileoverview class that contains a uid and a idToken for use when authenticating a user
+ */
+export class UserAuth {
+    
+    
+    /**
+     * A user's firebase Uid needed to compair to the uid decrypted from the idToken
+     * @date 8/13/2021 - 4:04:49 PM
+     *
+     * @type {String}
+     */
+    uid: string;
+
+    
+    /**
+     * firebase idToken, to be decrypted by firebase Auth to gain access to the firebase Auth User object
+     * @date 8/13/2021 - 4:05:23 PM
+     *
+     * @type {String}
+     */
+    idToken: string;
+
+    
+    /**
+     * Cross Sight Request Forgery token used to prevent someone stealing a cookie and sending it from a malicious domain 
+     * @date 8/13/2021 - 4:13:09 PM
+     *
+     * @type {string}
+     */
+    csrfToken?: string;
+
+    public constructor(uid: string, idToken: string){
+        this.uid = uid;
+        this.idToken = idToken;
+    }
+
+    
+    /**
+     * getter for the objects firebase uid
+     * @date 8/13/2021 - 4:15:19 PM
+     *
+     * @public
+     * @returns {string}
+     */
+    public getUid() {
+        return this.uid;
+    }
+
+    
+    /**
+     * getter for the objects firebase Id Token
+     * @date 8/13/2021 - 4:15:40 PM
+     *
+     * @public
+     * @returns {string}
+     */
+    public getIdToken() {
+        return this.idToken;
+    }
+
+    
+    /**
+     * Setter for the csrfToken property that is not set by the constructor
+     * @date 8/13/2021 - 4:15:59 PM
+     *
+     * @public
+     * @param {string} csrfToken
+     */
+    public setCsrfToken(csrfToken: string):void {
+        this.csrfToken = csrfToken;
+    }
+
+    
+}
