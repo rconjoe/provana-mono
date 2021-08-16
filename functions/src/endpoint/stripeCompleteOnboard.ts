d8263484ffb2270a7b26d90d8b2d2c600d6db75c
Joe Carlton <joe@rconjoe.com>
2021-08-16 13:42:33 -0400

    init stripeCompleteOnboard endpoint

---
+++ functions/src/endpoint/stripeCompleteOnboard.ts
@@ -0,0 +1,11 @@
+import * as functions from 'firebase-functions'
+import { CreatorDBC } from '../dbc/CreatorDBC'
+import { StripeAccountService } from '../services/stripe/StripeAccountService'
+
+export const stripeCompleteOnboard = functions.https.onCall(async (data) => {
+  const uid: string = data.uid
+  const creator = new CreatorDBC(uid)
+  const accountId = await creator.fetchAccountId()
+  const complete = await new StripeAccountService().verifyAccountCapabilities(accountId)
+  return await creator.completeOnboard(complete)
+})
