e5c2e3bc709bc4613c454fc71b38caf01727c2c8
Joe Carlton <joe@rconjoe.com>
2021-08-17 14:39:17 -0400

    working invitations

--- functions/src/dbc/InvitationDBC.ts
+++ functions/src/dbc/InvitationDBC.ts
@@ -26,7 +26,7 @@ const converter = {
   }
 }
 
-export class InvitationDBC {
+export class InvitationDBC extends Invitation {
 
   id: string | undefined
   code: string | undefined
@@ -36,34 +36,15 @@ export class InvitationDBC {
   ref: FirebaseFirestore.DocumentReference | undefined
 
   constructor(
-  id?: string | undefined,
-  code?: string | undefined,
-  generated?: number | undefined,
-  valid?: boolean | undefined,
-  uid?: string | undefined,
-  ref?: FirebaseFirestore.DocumentReference | undefined
+    id?: string,
+    code?: string,
+    generated?: number,
+    valid?: boolean,
+    uid?: string,
+    ref?: FirebaseFirestore.DocumentReference,
   ) {
-    this.id = id,
-    this.code = code,
-    this.generated = generated,
-    this.valid = valid,
-    this.uid = uid
+    super(id, code, generated, valid, uid)
     this.ref = ref
-  }
-
-  public async isValid(code?: string | undefined): Promise<boolean> {
-    code ? code : this.code
-    return this.valid ? this.valid : (await this.getFromCode(code)).valid!
-  }
-
-  public async createNew(dUser: string): Promise<InvitationDBC> {
-    this.ref = db.collection('invitations').doc(dUser)
-    this.code = this.generateCode()
-    this.generated = new UnixTimestampService().generate()
-    this.valid = true,
-    this.uid = ""
-    await this.ref.withConverter(converter).set(this)
-    return this
   }
 
   public toModel(): Invitation {
@@ -76,13 +57,55 @@ export class InvitationDBC {
     )
   }
 
-  public async getFromId(): Promise<InvitationDBC> {
-    if (this.id === null || this.id === undefined) throw new Error('Discord ID is required to fetch invitation.')
-    const snap = await db.collection('invitations').doc(this.id).withConverter(converter).get()
-    if (!snap.exists) throw new Error('Invitation not found in Firestore.')
-    return snap.data()!
+  public setUser(discordUserID: string): InvitationDBC {
+    this.id = discordUserID
+    return this
   }
 
+  public async getOrCreate(): Promise<string> {
+    if (this.id === null || this.id === undefined) throw new Error('Discord ID is required to get or create')
+    return (await this.exists()) ? await this.getFromId() : await this.create()
+  }
+
+  public async exists(): Promise<boolean> {
+    const snap = await db.collection('invitations').doc(this.id!).get()
+    return snap.exists
+  }
+
+  public async getFromId(): Promise<string> {
+    if (this.id === null || this.id === undefined) throw new Error('Discord ID is required to fetch invitation.')
+    const snap = await db.collection('invitations').doc(this.id).withConverter(converter).get()
+    return snap.data()!.code!
+  }
+
+  public async create(): Promise<string> {
+    this.ref = db.collection('invitations').doc(this.id!)
+    this.code = this.generateCode()
+    this.generated = new UnixTimestampService().generate()
+    this.valid = true,
+    await this.ref.withConverter(converter).set(this)
+    return this.code
+  }
+
+  private generateCode(): string {
+    const code = []
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    const len = characters.length
+    for (let i=0; i<10; i++) {
+      code.push(characters.charAt(Math.floor(Math.random() * len)))
+    }
+    return code.join('')
+  }
+
+  public setCode(code: string): InvitationDBC {
+    this.code = code
+    return this
+  }
+
+  public async isValid(code?: string | undefined): Promise<boolean> {
+    code ? code : this.code
+    return this.valid ? this.valid : (await this.getFromCode(code)).valid!
+  }
   public async associate(creator: Creator): Promise<FirebaseFirestore.WriteResult> {
     if (!creator.uid || !creator.code) throw new Error('Code and UID needed to associate Invitation with user.')
     const q = await db.collection('invitations').where('code', '==', creator.code).get()
@@ -100,18 +123,10 @@ export class InvitationDBC {
     return invitation.docs[0].data()
   }
 
-  private generateCode(): string {
-    const code = []
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    const len = characters.length
-    for (let i=0; i<10; i++) {
-      code.push(characters.charAt(Math.floor(Math.random() * len)))
-    }
-    return code.join('')
+
+  public async validate(code?: string | undefined): Promise<boolean> {
+    if (this.code === null || this.code === undefined) throw new Error('No code to validate!')
+    return await this.isValid(this.code)
   }
 
-  public async existsInDatabase(): Promise<boolean> {
-    const snap = await db.collection('invitations').doc(this.id!).get()
-    return snap.exists
-  }
 }
