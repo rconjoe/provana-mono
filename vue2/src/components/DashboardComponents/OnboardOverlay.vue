a9669aeaa8cfaa5de09ec1f2195e51523858596f
Joe Carlton <joe@rconjoe.com>
2021-08-20 16:43:39 -0400

    this component shows if a creator is logged in and not onboarded. the button on it calls our API to generate an account link and then redirects to it. you land back on /onboardSuccess hopefully.

---
+++ vue2/src/components/DashboardComponents/OnboardOverlay.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <LoadingOverlay v-if="onboarded === false && redirectLoading === true" />
+  <h1>Get paid up to 15 days faster with Stripe and Provana.GG</h1>
+  <h3>
+    Wish your customers could pay you on the spot? We’ve teamed up with Stripe so you can accept online invoice payments via credit card, debit card, or Apple Pay, to get the cash flowing in without having to go to great lengths
+  </h3>
+  <v-btn class="btnCTA mt-4" color="primary" @click="generateLink"> Get Started </v-btn>
+</div>
+</template>
+
+<script>
+import { functions } from '../../plugins/firebase'
+import LoadingOverlay from '../LoadingOverlay.vue'
+export default {
+  name: 'OnboardOverlay',
+  components: {
+    LoadingOverlay
+  },
+
+  data: () => ({
+    redirectLoading: false
+  }),
+
+  methods: {
+    async generateLink() {
+      this.redirectLoading = true
+      const stripeAccountOnboard = functions.httpsCallable('stripeAccountOnboard')
+      const response = await stripeAccountOnboard({ uid: this.$user.uid })
+        console.log(response)
+        window.location = response.data.url
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
