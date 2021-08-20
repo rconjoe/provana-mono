6e06c3b3ab7c7f56b8e118820073197cba8e9ca6
dustingits <dustinderouen@me.com>
2021-08-20 11:40:11 -0500

    dashboard onboard update

---
+++ vue2/src/components/ReusableComponents/Overlay.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="wrapper">
+		<v-overlay class="overlay" color="#111111" :value="show" style="z-index:9" opacity=".97">
+		<slot>
+			content
+		</slot>
+    </v-overlay>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'overlay',
+        props: {
+            show:{
+                default:false,
+                type: Boolean,
+            }
+        },
+		data: () => ({
+			
+		}),
+	}
+</script>
+
+<style scoped>
+ >>>.v-overlay__content{
+   width: 80%;
+ }
+  .waitText{
+    text-align: center;
+    font: normal bold 1.5vw Poppins;
+    color: white;
+  }
+	.wrapper {
+		z-index: 9;
+    width: 100vw;
+	}
+	.bar {
+		position: center center;
+	}
+</style>
--- vue2/src/views/Dashboard.vue
+++ vue2/src/views/Dashboard.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <!--Conatiner row. -->
     <v-row class="pa-0  dashRow">
       <!-- Dashboard nav drawer must sit outside of the column -->
+      <!-- Creator and supporter links are set by checking claim then displaying a limited or full list of links -->
       <div v-if="!$vuetify.breakpoint.mobile" cols="1" class="pa-0 navDrawerCol">
         <DashboardNavDrawer
           style="height:100%  "
-          v-if="!$vuetify.breakpoint.mobile"
-          :links=" !seller ? buyerLinks : !onboarded ? onboardLinks : sellerLinks"
+          :links="this.$store.state.auth.claims.type"
           :window="window"
           @update-window="updateWindow"
           :avatar="profile.avatar"
         />
       </div>
+      <!-- Onboard Overlay -->
+      <Overlay :show="false">
+          <DashboardNotOnboarded />
+      </Overlay>
 
       <!-- Column wraps all dashboard windows. -->
       <v-col class="pa-0 DashCol">
@@ -21,9 +24,7 @@
           <!-- home window-->
           <v-window-item name="DashHomeWindow" class="dashWindow">
             <v-card color="transparent" name="DashHomeCard" dark flat class="dashCard">
-
-              <dashboard-home v-if="!seller ? true : onboarded ? true : false" :profile="profile"></dashboard-home>
-              <DashboardNotOnboarded v-else/>
+              <dashboard-home :profile="profile"></dashboard-home>
             </v-card>
           </v-window-item>
           <!-- account window -->
@@ -53,22 +54,6 @@
               <dashboard-contact @update-notifications="updateNotifications"></dashboard-contact>
             </v-card>
           </v-window-item>
-          <!-- requests card
-                    <v-window-item>
-                    <v-card>
-                        <v-card-text>
-                            <v-list-item-avatar color="grey lighten-1">
-                            </v-list-item-avatar>
-                            <v-list-item-content>
-                                <v-list-item-title> request card </v-list-item-title>
-                                <v-list-item-text>
-                                    This is a test card.
-                                </v-list-item-text>
-                            </v-list-item-content>
-                        </v-card-text>
-                    </v-card>
-                    </v-window-item>
-                    requests card -->
         </v-window>
       </v-col>
     </v-row>
@@ -87,7 +72,8 @@ import DashboardPayments from '@/components/DashboardComponents/DashboardPayment
 import DashboardContact from '@/components/DashboardComponents/DashboardContact.vue';
 import DashboardNavDrawer from '@/components/DashboardComponents/DashboardNavDrawer.vue';
 import DashboardNavMobile from '@/components/DashboardComponents/DashboardNavMobile.vue';
-import DashboardNotOnboarded from '@/components/DashboardComponents/DashboardNotOnboarded.vue'
+import DashboardNotOnboarded from '@/components/DashboardComponents/DashboardNotOnboarded.vue';
+import Overlay from '@/components/ReusableComponents/Overlay.vue';
 
 export default {
   name: 'Dashboard',
@@ -101,14 +87,14 @@ export default {
     DashboardContact,
     DashboardNavMobile,
     DashboardNavDrawer,
-    DashboardNotOnboarded
+    DashboardNotOnboarded,
+    Overlay
   },
 
   data: () => ({
     profile: {},
     drawer: null,
     window: 0,
-    seller:null,
     onboarded:true,
     id:null,
     supporters: [
@@ -179,6 +165,16 @@ export default {
       this.profile = profile.data()
     })
   },
+  computed:{
+    notOnboarded(){
+      if( this.$store.state.auth.claims.type === 'creators' && this.profile.onboarded === false){
+        return true
+      }
+      else{
+        return false
+      }
+    }
+  }
 };
 </script>
 
