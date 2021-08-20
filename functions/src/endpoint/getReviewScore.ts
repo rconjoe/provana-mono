e9fa057a8f843b66da996b91b60228876c6f4291
unknown <michaelkimcoen@gmail.com>
2021-08-20 13:40:40 -0400

    created the endpoint to get the averaged review score

---
+++ functions/src/endpoint/getReviewScore.ts
@@ -0,0 +1,8 @@
+import * as functions from 'firebase-functions';
+import { ReviewDBC } from '../dbc/ReviewDBC';
+
+export const getReviewScore = functions.https.onCall(async (data, context) => {
+    const dbc = new ReviewDBC().setSellerUid(data.sellerUid)
+
+    return await dbc.getReviewScore()
+})
