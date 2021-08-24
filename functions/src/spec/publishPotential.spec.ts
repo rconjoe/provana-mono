9c4542059f5413e8253209f446d9511ea6e5cbbc
Joe Carlton <joe@rconjoe.com>
2021-08-24 17:13:02 -0400

    working publishPotential.ts test

--- functions/src/spec/publishPotential.spec.ts
+++ functions/src/spec/publishPotential.spec.ts
@@ -10,97 +10,32 @@ describe('Tests publishPotential HTTPS endpoint', () => {
   })
 
   afterEach(async () => {
-    // const srv = await db.collection('services').where('uid', '==', '123abc').get()
-    // await srv.docs[0].ref.delete()
-    // const ses = await db.collection('sessions').where('sellerUid', '==', '123abc').get()
-    // ses.forEach(doc => doc.ref.delete())
   })
 
-  afterAll(() => {
+  afterAll(async () => {
     testEnv.cleanup()
+    await db.collection('sessions').doc('12345').delete()
   })
 
-  it('Publishes potential sessions with 1 attendee', async () => {
-    await db.collection('services').doc('123').set({
-      active: true,
-      attendees: 1,
-      color: '00d6ff',
-      id: '123',
-      mandatoryFill: true,
-      serviceCost: 25,
-      serviceDescription: 'jesty testy',
-      serviceLength: 90,
-      serviceName: 'jestington testington',
-      sessionDocIdArray: ['12345'],
-      software: 'onlyfans',
-      stripePrice: 'price_12345',
-      tags: ['booty', 'cats', 'womp'],
-      uid: '123abc'
-    })
+  it('Sets any potential session to published and update its color', async () => {
     await db.collection('sessions').doc('12345').set({
-      color: 'grey',
-      end: 139476827,
-      id: '12345',
-      mandatoryFill: true,
-      name: 'jestest',
       sellerUid: '123abc',
-      serviceColor: '#00d6ff',
-      serviceDocId: '123',
       slots: 1,
-      start: 1129387455,
-      status: 'potential'
-    })
-    const wrapped = testEnv.wrap(api.publishPotential)
-    const response = await wrapped({uid: '123abc'})
-    console.log(response)
-
-    const sessionRef = await db.collection('sessions').doc('12345').get()
-    const s = sessionRef.data()!
-    expect(s.status).toBe('published')
-    expect(s.color).toBe('#00d6ff')
-  })
-  it('Publishes potential session with multiple slots', async () => {
-    await db.collection('services').doc('456').set({
-      active: true,
-      attendees: 5,
-      color: '00d6ff',
-      id: '456',
+      serviceDocId: '67890',
       mandatoryFill: true,
-      serviceCost: 25,
-      serviceDescription: 'jesty testy',
-      serviceLength: 90,
-      serviceName: 'jestington testington',
-      sessionDocIdArray: [],
-      software: 'onlyfans',
-      stripePrice: 'price_12345',
-      tags: ['booty', 'cats', 'womp'],
-      uid: '123abc'
-  })
-    await db.collection('sessions').doc('45678').set({
+      name: 'jest-test',
       color: 'grey',
-      end: 139476827,
-      id: '45678',
-      mandatoryFill: true,
-      name: 'jestest',
-      sellerUid: '123abc',
-      serviceColor: '#00d6ff',
-      serviceDocId: '456',
-      slots: 5,
-      start: 1129387455,
+      serviceColor: '#FFC0CB',
+      start: 12345678,
+      end: 23456789,
+      id: '12345',
       status: 'potential'
     })
-
     const wrapped = testEnv.wrap(api.publishPotential)
     const response = await wrapped({uid: '123abc'})
     console.log(response)
-
-    const _parent = await db.collection('sessions').doc('45678').get()
-    const parent = _parent.data()!
-    expect(parent.status).toBe('published')
-    const slots = await db.collection('sessions').doc('45678').collection('slots').get()
-    expect(slots.size).toBe(5)
-    slots.forEach(slot => {
-      expect(slot.data()!.serviceDocId).toBe('456')
-    })
+    const q = await db.collection('sessions').where('sellerUid', '==', '123abc').get()
+    expect(q.docs[0].data().status).toBe('published')
+    expect(q.docs[0].data().color).toBe('#FFC0CB')
   })
 })
