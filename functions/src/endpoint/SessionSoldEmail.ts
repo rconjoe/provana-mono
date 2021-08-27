fdeb3c8dfad90a00087e381a0c75c26ae9247867
MrKim <mrkim@MrKims-MacBook-Air.local>
2021-08-27 15:32:37 -0400

    updated service code, and created an endpoint. Code needs to be reviewed before merged into Master

---
+++ functions/src/endpoint/SessionSoldEmail.ts
@@ -0,0 +1,17 @@
+import * as functions from "firebase-functions";
+import { sendSessionSoldEmail } from "../services/mailer/SessionSold";
+
+export const onSessionSold = functions.firestore.document('sessions/{sessionId}/slots/{slotId}').onUpdate(async(change, context) => {
+    const afterChange = change.after.data();
+    const beforeChange = change.before.data();
+
+    if(beforeChange.status === 'potential' && afterChange.status === 'booked') {
+        const today = new Date;
+        await sendSessionSoldEmail(afterChange.sellerUsername, afterChange.buyerUsername,today,afterChange.serviceDocId,afterChange.sellerUid);
+        return "Email Sent";
+    }
+    else {
+        return "Slot doc not updated this function should not have triggered";
+    }
+
+})
