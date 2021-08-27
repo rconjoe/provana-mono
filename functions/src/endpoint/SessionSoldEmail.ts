562eaa698633d2a13d1e6eb2575ec0629bd03645
MrKim <mrkim@MrKims-MacBook-Air.local>
2021-08-27 15:34:19 -0400

    updated the conditional clause so it actually works in our db

--- functions/src/endpoint/SessionSoldEmail.ts
+++ functions/src/endpoint/SessionSoldEmail.ts
@@ -5,7 +5,7 @@ export const onSessionSold = functions.firestore.document('sessions/{sessionId}/
     const afterChange = change.after.data();
     const beforeChange = change.before.data();
 
-    if(beforeChange.status === 'potential' && afterChange.status === 'booked') {
+    if(beforeChange.status === 'potential' && afterChange.status === 'purchased') {
         const today = new Date;
         await sendSessionSoldEmail(afterChange.sellerUsername, afterChange.buyerUsername,today,afterChange.serviceDocId,afterChange.sellerUid);
         return "Email Sent";
