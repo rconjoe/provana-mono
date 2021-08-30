a509b66cb071394052b27bc4d376f2f9f65cdc78
Michael Coen <michaelkimcoen@gmail.com>
2021-08-30 10:45:42 -0400

    added endpoint to index.ts

--- functions/src/endpoint/sendGenericEmail.ts
+++ functions/src/endpoint/sendGenericEmail.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 import { GenaricEmail } from '../services/mailer/GenericEmail';
 
-export const sendGenaricEmail = functions.https.onCall( async (data, context) => {
+export const sendGenericEmail = functions.https.onCall( async (data, context) => {
     return GenaricEmail(data.emailAddress, data.subject, data.body, data.html, data.templateId, data.dynamicTemplateData);
 })
