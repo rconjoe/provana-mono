82beb0b89a9f44e21ffe063298d7ac66b77b0c86
Joe Carlton <joe@rconjoe.com>
2021-09-01 16:27:23 -0400

    update SlotSold email template interface to match what sg expects

--- functions/src/services/mailer/MailService.ts
+++ functions/src/services/mailer/MailService.ts
@@ -14,20 +14,15 @@ export default class MailService {
       templateId: 'd-f644bc8af7284c169f1fc8e5535f65e7',
       dynamicTemplateData: {...data}
     })
-    if (response[0].statusCode == 200) return 'Mail sent,'
+    if (response[0].statusCode === 202) return 'Mail sent,'
     else throw new Error(`${response[0].statusCode}: ${response[0].body}`)
   }
 }
-/**
- * every mail template will have its own method on MailService
- * which expects a (probably inline) typed object matching
- * whatever data is needed for the template.
- */
 
 interface SlotSold {
-  serviceName: string,
-  sellerUsername: string,
-  buyerUsername: string,
+  username: string,
+  service: string,
+  buyer: string,
   time: string,
   date: string
 }
