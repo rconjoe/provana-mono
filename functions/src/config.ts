64ef706eebc211216247723d09fb091541e785ab
Joe Carlton <joe@rconjoe.com>
2021-09-02 14:12:23 -0400

    point at stripe-mock server :D

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -13,7 +13,10 @@ db.settings(settings)
 // stripe
 export const stripeSecret = "sk_test_51HJUgfGoIl5NLNcQ8xzPwo3tXqwoaGym8ZXwPBxbVWuOEEdCQxst4ORTV9x8GU4k4TK9uyFFiB9zLHMvDMLSV9UW00N8C4ejVB"
 export const stripe = new Stripe(stripeSecret, {
-  apiVersion: '2020-08-27'
+  apiVersion: '2020-08-27',
+  host: 'localhost',
+  protocol: 'http',
+  port: '12111'
 })
 
 // sendgrid
