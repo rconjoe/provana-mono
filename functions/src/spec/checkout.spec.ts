b04636c8966777807ca53bb28567012384e887ea
Joe Carlton <joe@rconjoe.com>
2021-09-02 14:13:18 -0400

    works with mock server

--- functions/src/spec/checkout.spec.ts
+++ functions/src/spec/checkout.spec.ts
@@ -1,11 +1,10 @@
 import { testEnv } from './env.spec'
-import { db } from '../config'
 
 describe('Tests checkout session generation endpoint', () => {
   let api: any
 
   beforeAll(async () => {
-    api: require('../index.ts')
+    api = require('../index.ts')
   })
 
   afterAll(async () => {
@@ -13,5 +12,17 @@ describe('Tests checkout session generation endpoint', () => {
   })
 
   it('Returns a checkout session ID', async () => {
+    const wrapped = testEnv.wrap(api.checkout)
+    const response = await wrapped({
+      uid: 'abc123',
+      username: 'jestbuyer',
+      customer: 'cus_123',
+      account: 'acc_123',
+      price: 'price_123',
+      serviceCost: 50,
+      slotId: '67890',
+      sessionId: '12345'
+    })
+    console.log(response)
   })
 })
