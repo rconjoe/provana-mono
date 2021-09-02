0ce3951201ae7de62a5ee1f1955b7afe5a0eba92
Joe Carlton <joe@rconjoe.com>
2021-09-01 20:35:43 -0400

    init checkout endpoint

---
+++ functions/src/endpoint/onCall/checkout.ts
@@ -0,0 +1,27 @@
+import * as functions from 'firebase-functions'
+import StripeCheckoutService from '../../services/stripe/StripeCheckoutService'
+import SlotDBC from '../../dbc/SlotDBC'
+
+export const checkout = functions.https.onCall(async (data, context) => {
+  const checkout = await new StripeCheckoutService().createCheckout({
+    uid: data.uid,
+    username: data.username,
+    customer: data.customer,
+    account: data.account,
+    price: data.price,
+    serviceCost: data.serviceCost,
+    slotId: data.slotId,
+    sessionId: data.sessionId
+  })
+  const slot = await new SlotDBC().setPath(data.sessionId, data.slotId)
+  await slot.update({
+    status: 'holding',
+    paymentIntent: checkout.payment_intent,
+    buyerUid: data.buyerUid,
+    buyerUsername: data.buyerUsername
+  })
+
+  // schedule task here
+
+  return checkout.id
+})
