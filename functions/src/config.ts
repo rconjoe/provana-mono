14a9a221efa1d3c981bec9d65bdf6e2f6a3e585b
Joe Carlton <joe@rconjoe.com>
2021-09-03 10:02:21 -0400

    add env switching for stripe

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,5 +1,5 @@
 import * as admin from 'firebase-admin'
-// import * as functions from 'firebase-functions'
+import * as functions from 'firebase-functions'
 import Stripe from 'stripe'
 import * as sendgrid from '@sendgrid/mail'
 
@@ -11,13 +11,19 @@ const settings = { timestampsInSnapshots: true }
 db.settings(settings)
 
 // stripe
-export const stripeSecret = "sk_test_51HJUgfGoIl5NLNcQ8xzPwo3tXqwoaGym8ZXwPBxbVWuOEEdCQxst4ORTV9x8GU4k4TK9uyFFiB9zLHMvDMLSV9UW00N8C4ejVB"
-export const stripe = new Stripe(stripeSecret, {
-  apiVersion: '2020-08-27',
-  host: 'localhost',
-  protocol: 'http',
-  port: '12111'
-})
+export const stripeTestKey = "sk_test_51HJUgfGoIl5NLNcQ8xzPwo3tXqwoaGym8ZXwPBxbVWuOEEdCQxst4ORTV9x8GU4k4TK9uyFFiB9zLHMvDMLSV9UW00N8C4ejVB"
+export var stripe: Stripe
+  if (process.env.NODE_ENV === 'test') {
+    stripe = new Stripe(stripeTestKey, {
+      apiVersion: '2020-08-27',
+      host: 'localhost',
+      protocol: 'http',
+      port: '12111'
+    })
+  }
+  else stripe = new Stripe(functions.config().stripe.secret, {
+    apiVersion: '2020-08-27'
+  })
 
 // sendgrid
 sendgrid.setApiKey('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')
