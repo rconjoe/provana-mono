5881e776af568972f184f4dd5d08cbb1d57dbb3f
Joe Carlton <joe@rconjoe.com>
2021-09-03 10:04:39 -0400

    init working StripePaymentIntentService

---
+++ functions/src/services/stripe/StripePaymentIntentService.ts
@@ -0,0 +1,23 @@
+import Stripe from 'stripe'
+import { stripe } from '../../config'
+
+export default class StripePaymentIntentService {
+  public async retrieve(id: string): Promise<Stripe.PaymentIntent> {
+    return await stripe.paymentIntents.retrieve(id)
+    .catch(err => {
+      throw new Error(err)
+    })
+  }
+
+  public async status(id: string): Promise<string> {
+    const intent = await this.retrieve(id)
+    return intent.status
+  }
+
+  public async cancel(id: string): Promise<void> {
+    await stripe.paymentIntents.cancel(id)
+    .catch(err => {
+      throw new Error(err)
+    })
+  }
+}
