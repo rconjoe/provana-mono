8f3e496867dc130811f48ff3a34ee315627ffa36
Joe Carlton <joe@rconjoe.com>
2021-09-07 15:05:44 -0400

    Profile fetching is now an action that performs a snapshot bind. timezone is also in here now.

--- vue2/src/store/auth.js
+++ vue2/src/store/auth.js
@@ -7,54 +7,58 @@ dayjs.extend(utc)
 dayjs.extend(timezone)
 
 export const auth = {
-    namespaced: true,
+  namespaced: true,
 
-    state: () => ({
-        currentUser: null,
-        error: null,
-        claims: null,
-        tz: null
-    }),
+  state: () => ({
+    currentUser: null,
+    error: null,
+    claims: null,
+    tz: null
+  }),
 
-    mutations: {
-        SET_USER(state, data) {
-            state.currentUser = data;
-        },
-        SET_ERROR(state, data) {
-            state.error = data;
-        },
-        SET_CLAIMS(state, data) {
-            state.claims = data;
-        },
-        SET_TIMEZONE(state, data) {
-            state.tz = data;
-        }
+  mutations: {
+    SET_USER(state, data) {
+      state.currentUser = data;
     },
-    actions: {
-        async fetchUserProfile({ commit }, uid) {
-            if (!store.state.auth.claims.type) return
-            const _profile = await db.collection(store.state.auth.claims.type).doc(uid).get()
-            commit('SET_USER', _profile.data())
-        },
-        async setTimezone({ commit }) {
-            if (!store.state.currentUser) {
-                const guess = dayjs.tz.guess()
-                commit('SET_TIMEZONE', guess)
-            }
-            else {
-                const tz = store.state.currentUser.timezone
-                if (!tz || tz === '') {
-                    const guess = dayjs.tz.guess()
-                    dayjs.tz.setDefault(guess)
-                    commit('SET_TIMEZONE', guess)
-                    await db.collection(store.state.auth.claims.type).doc(store.state.auth.currentUser.uid)
-                    .set({ timezone: guess }, { merge: true })
-                }
-                else {
-                    dayjs.tz.setDefault(tz)
-                    commit('SET_TIMEZONE', tz)
-                }
-            }
-        }
+    SET_ERROR(state, data) {
+      state.error = data;
+    },
+    SET_CLAIMS(state, data) {
+      state.claims = data;
+    },
+    SET_TIMEZONE(state, data) {
+      state.tz = data;
     }
+  },
+  actions: {
+    async bindProfile({ commit }, uid) {
+      if (!store.state.auth.claims.type) return
+      db
+        .collection(store.state.auth.claims.type)
+        .doc(uid)
+        .onSnapshot((snap) => {
+          commit('SET_USER', snap.data())
+        })
+    },
+    async setTimezone({ commit }) {
+      if (!store.state.currentUser) {
+        const guess = dayjs.tz.guess()
+        commit('SET_TIMEZONE', guess)
+      }
+      else {
+        const tz = store.state.currentUser.timezone
+        if (!tz || tz === '') {
+          const guess = dayjs.tz.guess()
+          dayjs.tz.setDefault(guess)
+          commit('SET_TIMEZONE', guess)
+          await db.collection(store.state.auth.claims.type).doc(store.state.auth.currentUser.uid)
+            .set({ timezone: guess }, { merge: true })
+        }
+        else {
+          dayjs.tz.setDefault(tz)
+          commit('SET_TIMEZONE', tz)
+        }
+      }
+    }
+  }
 }
