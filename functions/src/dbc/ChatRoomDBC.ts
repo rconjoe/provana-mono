4a8e6aca5e11f0b65562837ad30e01b93e8e94e0
Joe Carlton <joe@rconjoe.com>
2021-09-08 14:23:04 -0400

    init ChatRoomDBC. its 'initialize' method takes the id of the parent session and the creator's uid. session id's and chat room id's match for ease in querying - whoever's on the session gets to query the chat. converter is written but not used yet.

---
+++ functions/src/dbc/ChatRoomDBC.ts
@@ -0,0 +1,54 @@
+import ChatRoom from '../models/ChatRoom'
+import { db, addToArray } from '../config'
+
+// const converter: FirebaseFirestore.FirestoreDataConverter = {
+//   toFirestore(r: ChatRoomDBC): FirebaseFirestore.DocumentData {
+//     return {
+//       users: r.users ? r.users : [],
+//       creator: r.creator ? r.creator : "",
+//       messages: r.messages ? r.messages : FirebaseFirestore.CollectionReference
+//     }
+//   },
+//   fromFirestore(snap: FirebaseFirestore.QueryDocumentSnapshot): ChatRoomDBC {
+//     const data = snap.data()
+//     return new ChatRoomDBC(
+//       data.users,
+//       data.creator,
+//       snap.ref.collection('messages'),
+//       snap.ref
+//     )
+//   }
+// }
+
+export default class ChatRoomDBC extends ChatRoom {
+  messages: FirebaseFirestore.CollectionReference | undefined
+  ref: FirebaseFirestore.DocumentReference | undefined
+
+  constructor(
+    users?: Array<string>,
+    creator?: string,
+    messages?: FirebaseFirestore.CollectionReference,
+    ref?: FirebaseFirestore.DocumentReference
+  ) {
+    super(users, creator)
+    this.messages = messages
+    this.ref = ref
+  }
+
+  public async initialize(data: {
+    id: string,
+    creator: string
+  }): Promise<void> {
+    this.ref = db
+      .collection('chats')
+      .doc(data.id)
+    await this.ref.set({
+      creator: data.creator
+    })
+    await addToArray({
+      ref: this.ref,
+      field: 'users',
+      value: data.creator
+    })
+  }
+}
