25bb487e060d2bfaa8adef9c24fb6cb771f7e114
Joe Carlton <joe@rconjoe.com>
2021-09-10 17:28:49 -0400

    add methods for scheduling slot-start and session-start tasks

--- functions/src/services/TaskService.ts
+++ functions/src/services/TaskService.ts
@@ -17,31 +17,44 @@ export default class TaskService {
     else this.client = new CloudTasksClient()
   }
 
-  public async onCheckout(slotId: string): Promise<void> {
+  public async onCheckout(slotId: string): Promise<string> {
     return await this.schedule(this.buildRequest('no-abandoned-checkouts', slotId))
   }
 
+  public async scheduleSlotStart(slotId: string, secondsUntil: number): Promise<string> {
+    return await this.schedule(this.buildRequest('slot-start', slotId, secondsUntil))
+  }
+
+  public async scheduleSessionStart(sessionId: string, secondsUntil: number): Promise<string> {
+    return await this.schedule(this.buildRequest('session-start', sessionId, secondsUntil))
+  }
+
   private async schedule(request: protos.google.cloud.tasks.v2beta3.ICreateTaskRequest)
-  : Promise<void> {
-    await this.client!.createTask(request)
+  : Promise<string> {
+    const [ task ] = await this.client!.createTask(request)
     .catch(err => {
       throw new Error(err)
     })
+    return task.name!
   }
 
-  private buildRequest(type: string, payload: string)
+  private buildRequest(type: string, payload: string, secondsUntil?: number)
   : protos.google.cloud.tasks.v2beta3.ICreateTaskRequest {
     let url: string
     let seconds: number
     const parent = this.client!.queuePath('db-abstract', 'us-central1', type)
     switch (type) {
       case 'no-abandoned-checkouts':
-        url = 'https://www.google.com'
+        url = 'https://us-central1-db-abstract.cloudfunctions.net/confirmCheckoutComplete'
         seconds = 300 + Date.now() / 1000
         break
-      case 'capture-intent':
-        url = 'wow.com'
-        seconds = 10
+      case 'slot-start':
+        url = 'https://google.com'
+        seconds = secondsUntil!
+        break
+      case 'session-start':
+        url = 'https://google.com'
+        seconds = secondsUntil!
         break
       // ...
       default:
