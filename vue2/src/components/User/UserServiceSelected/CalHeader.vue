e1424037db34e8a1b801af4579957a6b9b596358
dustingits <dustinderouen@me.com>
2021-09-14 10:46:50 -0500

    storefront components update

---
+++ vue2/src/components/User/UserServiceSelected/CalHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-sheet height="40px" color="transparent">
+					<h1 class="calHeaderText"> Pick the right spot </h1>
+					<v-btn fab text color="grey darken-2" @click="prev">
+						<v-icon>
+							fas fa-chevron-left
+						</v-icon>
+					</v-btn>
+
+					<v-btn fab text color="grey darken-2" @click="next">
+						<v-icon>
+							fas fa-chevron-right
+						</v-icon>
+					</v-btn>
+				</v-sheet>
+</template>
+
+<script>
+export default {
+    methods:{
+        prev(){
+            this.$emit('prev')
+        },
+        next(){
+            this.$emit('next')
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+	.calHeaderText {
+		font: normal 600 1.5625vw Poppins;
+		letter-spacing: -0.078125vw;
+		display: inline-block;
+	}
+
+</style>
