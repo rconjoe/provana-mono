8762287911380646493e67ba9cf884b777e9b1f5
Michael Coen <michaelkimcoen@gmail.com>
2021-09-16 16:44:48 -0400

    added jsdoc code

--- functions/src/services/stripe/StripeCheckoutService.ts
+++ functions/src/services/stripe/StripeCheckoutService.ts
@@ -1,7 +1,38 @@
 import Stripe from 'stripe'
 import { stripe } from '../../config'
 
+
+/**
+ * Export for the StripeCheckoutService class
+ *
+ * @class StripeCheckoutService
+ * @typedef {StripeCheckoutService}
+ * @module StripeCheckoutService
+ * @category src
+ * @subcategory services/stripe
+ */
 export default class StripeCheckoutService {
+
+  
+  /**
+   *  Creates the session object for the user to check out by calling the Stripe Checkout api and takes an object with the uid, username, Creator's Stripe custmer id, Creator's Stripe account id,
+   *  the price on Stripe's end, cost of the service, the Firestore slot document id, and the Firestore session document id.
+   *  
+   *
+   * @public
+   * @async
+   * @param {{
+      uid: string,
+      username: string,
+      customer: string,
+      account: string,
+      price: string,
+      serviceCost: number,
+      slotId: string,
+      sessionId: string
+    }} data
+   * @returns {Promise<Stripe.Checkout.Session>}
+   */
   public async createCheckout(data: {
     uid: string,
     username: string,
@@ -46,11 +77,28 @@ export default class StripeCheckoutService {
     })
   }
 
+  
+  /**
+   * Takes the cost of the service and converts it to the cost in pennies, and then returns 20% of that number
+   *
+   * @private
+   * @param {number} serviceCost
+   * @returns {number}
+   */
   private fee(serviceCost: number): number {
     const cents = serviceCost * 100
     return cents * 0.2 // 20%
   }
 
+  
+  /**
+   * Calls the Stripe Checkout api and returns the checkout session object
+   *
+   * @public
+   * @async
+   * @param {string} id
+   * @returns {unknown}
+   */
   public async retrieve(id: string) {
     return await stripe.checkout.sessions.retrieve(id)
       .catch(err => {
