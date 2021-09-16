4ff22e11fa09ca9c74ff0d1c688affb83cf9b059
Michael Coen <michaelkimcoen@gmail.com>
2021-09-16 16:25:09 -0400

    added jsdoc code

--- functions/src/services/stripe/StripeAccountService.ts
+++ functions/src/services/stripe/StripeAccountService.ts
@@ -2,7 +2,28 @@ import Creator from '../../models/Creator'
 import { stripe } from '../../config'
 import Stripe from 'stripe'
 
+
+
+/**
+ * Export for the StripeAccountService class
+ *
+ * @class StripeAccountService
+ * @typedef {StripeAccountService}
+ * @module StripeAccountService
+ * @category src
+ * @subcategory services/stripe
+ */
 export default class StripeAccountService {
+
+  
+  /**
+   * Takes a Creator object and calls the Stripe Accounts api to create a new stripe account and sets the account property on the object with the account id return from stripe
+   *
+   * @public
+   * @async
+   * @param {Creator} _newAccount
+   * @returns {Promise<Creator>}
+   */
   public async create(_newAccount: Creator): Promise<Creator> {
     const account = await stripe.accounts.create({
       country: 'US',
@@ -21,6 +42,16 @@ export default class StripeAccountService {
     return _newAccount
   }
 
+  
+  /**
+   * takes a Stripe account id and sets the callback and retry url's and returns the url to continue on with the stripe onboarding flow.
+   * Stripe docs: https://stripe.com/docs/api/account_links/create
+   *
+   * @public
+   * @async
+   * @param {string} accountId
+   * @returns {Promise<Stripe.Response<Stripe.AccountLink>>}
+   */
   public async generateAccountLink(accountId: string): Promise<Stripe.Response<Stripe.AccountLink>> {
     return await stripe.accountLinks.create({
       account: accountId,
@@ -30,6 +61,15 @@ export default class StripeAccountService {
     })
   }
 
+  
+  /**
+   * Calls stripe Accounts api to retrieve the details of the stripe account
+   *
+   * @public
+   * @async
+   * @param {string} accountId
+   * @returns {Promise<boolean>}
+   */
   public async verifyAccountCapabilities(accountId: string): Promise<boolean> {
     const account = await stripe.accounts.retrieve(accountId)
     console.log(account)
