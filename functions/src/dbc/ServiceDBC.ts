cd103a73135b4c10953c5f098aa95d6c974cae81
Michael Coen <michaelkimcoen@gmail.com>
2021-09-16 14:11:16 -0400

    added jsdoc code and removed an method that was not needed becuase you can just use the constructor instead

--- functions/src/dbc/ServiceDBC.ts
+++ functions/src/dbc/ServiceDBC.ts
@@ -1,6 +1,12 @@
 import { db } from '../config'
 import Service from '../models/Service'
 
+
+/**
+ * Converter for either mapping data to a Firestore document snapshot or from Firestore to ServiceDBC object
+ *
+ * @type {{ toFirestore(service: ServiceDBC): any; fromFirestore(snapshot: any): ServiceDBC; }}
+ */
 const converter = {
   toFirestore(service: ServiceDBC): FirebaseFirestore.DocumentData {
     return {
@@ -42,10 +48,42 @@ const converter = {
   }
 }
 
+
+/**
+ * Export for the ServiceDBC object
+ *
+ * @module Service
+ * @class ServiceDBC
+ * @typedef {ServiceDBC}
+ * @extends {Service}
+ * @category src
+ * @subcategory dbc
+ */
 export default class ServiceDBC extends Service {
 
   ref: FirebaseFirestore.DocumentReference | undefined
 
+  
+  /**
+   * Creates an instance of ServiceDBC.
+   *
+   * @constructor
+   * @param {?string} [id] Id of the Firestore document
+   * @param {?string} [serviceName] The name that the Creator gave the service
+   * @param {?string} [serviceDescription] The description of the service that the Creator gave it
+   * @param {?number} [serviceCost] Cost of the service in USD
+   * @param {?number} [serviceLength] The length of time that the service will take
+   * @param {?Array<string>} [tags] Key words given by the Creator, for use when searching for a Service
+   * @param {?string} [color] The color that the service will show up as on the calendar
+   * @param {?string} [software] The software that will be used during the session
+   * @param {?number} [attendees] Number of Supporters that can book a slot on the session
+   * @param {?boolean} [mandatoryFill] Boolean if all slots must be sold for a session to not be canceled 
+   * @param {?Array<string>} [sessionDocIdArray] Array of firebase document id's that point to all the sessions of this service
+   * @param {?string} [uid] Creator's Firebase uid
+   * @param {?string} [stripePrice] Stripe price id, from the Stripe Price api, sent during checkout to create a Stripe payment intent
+   * @param {?boolean} [active] Used by the front end, to toggle being able to see this service, only set to false when a Creator deletes a service that still has booked sessions
+   * @param {?FirebaseFirestore.DocumentReference} [ref] Firestore document reference that points to the proper document we want to retrive data from or edit
+   */
   constructor(
     id?: string,
     serviceName?: string,
@@ -82,6 +120,13 @@ export default class ServiceDBC extends Service {
     this.ref = ref
   }
 
+  
+  /**
+   * Method that takes the properties of the ServiceDBC and creates a new Service object out of them
+   *
+   * @public
+   * @returns {Service}
+   */
   public toModel(): Service {
     return new Service(
       this.id,
@@ -101,31 +146,14 @@ export default class ServiceDBC extends Service {
     )
   }
 
-  public callData(
-    serviceName: string,
-    serviceCost: number,
-    serviceDescription: string,
-    serviceLength: number,
-    attendees: number,
-    tags: Array<string>,
-    software: string,
-    mandatoryFill: boolean,
-    serviceColor: string,
-    uid: string
-  ): ServiceDBC {
-    this.serviceName = serviceName
-    this.serviceCost = serviceCost
-    this.serviceDescription = serviceDescription
-    this.serviceLength = serviceLength
-    this.attendees = attendees
-    this.tags = tags
-    this.software = software
-    this.mandatoryFill = mandatoryFill
-    this.color = serviceColor
-    this.uid = uid
-    return this
-  }
-
+  
+  /**
+   * Method that creates a new Firestoer document inside the 'services' collection and sets the id, and active property of the ServiceDBC object
+   *
+   * @public
+   * @async
+   * @returns {Promise<ServiceDBC>}
+   */
   public async initialize(): Promise<ServiceDBC> {
     this.ref = db.collection('services').doc()
     this.id = this.ref.id
@@ -137,6 +165,16 @@ export default class ServiceDBC extends Service {
     return this
   }
 
+  
+  /**
+   * Method that updates any field and value on a service on the Firestore document
+   *
+   * @public
+   * @async
+   * @param {string} field
+   * @param {*} value
+   * @returns {Promise<FirebaseFirestore.WriteResult>}
+   */
   public async update(field: string, value: any): Promise<FirebaseFirestore.WriteResult> {
     if (this.id === undefined || this.id === "") throw new Error('requires ID')
     return await db.collection('services').doc(this.id).update({
@@ -147,6 +185,16 @@ export default class ServiceDBC extends Service {
     })
   }
 
+  
+  /**
+   * Method that updates the price of a service on the Firestore document
+   * @date 9/16/2021 - 2:09:43 PM
+   *
+   * @public
+   * @async
+   * @param {string} price
+   * @returns {Promise<FirebaseFirestore.WriteResult>}
+   */
   public async updatePrice(price: string): Promise<FirebaseFirestore.WriteResult> {
     if (this.id === undefined || this.id === "") throw new Error('requires ID')
     return await db.collection('services').doc(this.id).update({
