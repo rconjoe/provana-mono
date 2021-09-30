70adba9a6a7e014ad782871349895ed8727de012
Joe Carlton <joe@rconjoe.com>
2021-09-30 15:07:14 -0400

    This was a partial rework of how the change is handled inside onUpdate functions at a high level. Instead of passing a whole change object to a StatusHandler (unclear and has lots of unused data), we get the before.status and case it out, calling a different method on the handler based on that. So now, the StatusHandler only need be constructed with an after.session, already converted to a DBC in the endpoint.

--- functions/src/endpoint/triggers/session-status/SessionStatusHandler.ts
+++ functions/src/endpoint/triggers/session-status/SessionStatusHandler.ts
@@ -1,4 +1,3 @@
-import { Change } from 'firebase-functions'
 import SlotDBC from '../../../dbc/SlotDBC'
 import SessionDBC from '../../../dbc/SessionDBC'
 import ChatRoomDBC from '../../../dbc/ChatRoomDBC'
@@ -8,96 +7,79 @@ import TimeService from '../../../services/TimeService'
 import NotificationDBC from '../../../dbc/NotificationDBC'
 
 export default class SessionStatusHandler {
-  before: SessionDBC | undefined
-  after: SessionDBC | undefined
+  session: SessionDBC 
+  
+  constructor(session: SessionDBC) {
+    this.session = session
+  }
 
-  public async run(change: Change<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>) {
-    this.before = this.toDbc(change.before)
-    this.after = this.toDbc(change.after)
-    const bStatus = this.before.status
-    const a = this.after!
-    const aStatus = a.status
-    if (bStatus === 'potential' && aStatus === 'published') {
-      this.onPublish()
-    }
-    else if (bStatus === 'published' && aStatus === 'full') {
-      this.onFull()
-    }
-    else if (bStatus === 'full' && aStatus === 'published') {
-      this.onSlotCancelWhenFull()
-    }
-    else if (bStatus === 'published' && aStatus === 'active') {
-      this.onActive();
-    }
-    else if (bStatus === 'full' && aStatus === 'active') {
-      this.onActive();
+  public async potential(): Promise<void> {
+    if (this.session.status === 'published') {
+      for (let i=1; i<=this.session.slots!; i++) {
+        await new SlotDBC(
+          "",
+          this.session.name,
+          i,
+          this.session.slots,
+          this.session.mandatoryFill,
+          this.session.start,
+          this.session.end,
+          this.session.sellerUid,
+          this.session.serviceDocId,
+          "",
+          "",
+          "",
+          'published',
+          this.session.id,
+          undefined
+        ).publish()
+      }
+      await new ChatRoomDBC().initialize({
+        id: this.session.id!,
+        creator: this.session.sellerUid!,
+        title: this.session.name!
+      })
     }
   }
 
-  private toDbc(snapshot: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>): SessionDBC {
-    const data = snapshot.data()
-    return new SessionDBC(
-      data.sellerUid,
-      data.slots,
-      data.booked,
-      data.serviceDocId,
-      data.mandatoryFill,
-      data.name,
-      data.color,
-      data.serviceColor,
-      data.start,
-      data.end,
-      snapshot.id,
-      data.status,
-      snapshot.ref
-    )
-  }
-
-  private async onPublish(): Promise<void> {
-    const a = this.after!
-    for (let i=1; i<=a.slots!; i++) {
-      await new SlotDBC(
-        "",
-        a.name,
-        i,
-        a.slots,
-        a.mandatoryFill,
-        a.start,
-        a.end,
-        a.sellerUid,
-        a.serviceDocId,
-        "",
-        "",
-        "",
-        'published',
-        a.id,
-        undefined
-      ).publish()
-    }
-    await new ChatRoomDBC().initialize({
-      id: a.id!,
-      creator: a.sellerUid!,
-      title: a.name!
-    })
-    return
-  }
-
-  private async onActive(): Promise<void> {
-    const a = this.after!
-    const notif = new NotificationDBC(a.sellerUid!,"Cancellation","A user has cancled their session with you, please check your dashboard",true);
-  }
-
-  private async onFull(): Promise<void> {
-    const a = this.after!
-    if (a.mandatoryFill === true) {
-      const secondsUntil = new TimeService().generate()
-      const task = await new TaskService().scheduleSessionStart(a.id!, secondsUntil)
-      await new TaskDBC(a.id!).write(task)
+  public async published(): Promise<void> {
+    switch (this.session.status) {
+      case 'active':
+        await new NotificationDBC(
+          this.session.sellerUid!,
+          'Starting',
+          'Your session is starting now!',
+          true
+        ).send()
+      case 'full':
+        if (this.session.mandatoryFill === true) {
+          const secondsUntil = new TimeService().generate()
+          const task = await new TaskService().scheduleSessionStart(this.session.id!, secondsUntil)
+          await new TaskDBC(this.session.id!).write(task)
+        }
+      break
     }
   }
 
-  private async onSlotCancelWhenFull(): Promise<void> {
-    const task = await new TaskDBC(this.after!.id!).retrieve()
-    await new TaskService().cancel(task)
+    public async full(): Promise<void> {
+      if (this.session.status === 'active') {
+        await new NotificationDBC(
+          this.session.sellerUid!,
+          'Starting',
+          'Your session is starting now!',
+          true
+        ).send()
+      }
+    }
+
+  public async active(): Promise<void> {
+    switch (this.session.status) {
+      case 'succeeded': 
+        // we still need this session to stick around for review purposes
+        break
+      case 'cancelled':
+        // decide how much of deletion and notif/mail handling goes here (ideally most/all of it)
+        break
+    }
   }
 }
