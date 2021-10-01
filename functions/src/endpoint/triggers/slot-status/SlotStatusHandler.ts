163a5938527eff001a34b11ddbd532dbda97dd47
Joe Carlton <joe@rconjoe.com>
2021-09-30 20:28:48 -0400

    in which onSessionUpdate and SessionStatusHandler are updated to reflect the cleaner logic and no-baggage-data to handler :)

--- functions/src/endpoint/triggers/slot-status/SlotStatusHandler.ts
+++ functions/src/endpoint/triggers/slot-status/SlotStatusHandler.ts
@@ -1,122 +1,112 @@
-import { Change } from 'firebase-functions'
 import SlotDBC from '../../../dbc/SlotDBC'
 import SessionDBC from '../../../dbc/SessionDBC'
 import ChatRoomDBC from '../../../dbc/ChatRoomDBC'
-import TimeService from '../../../services/TimeService'
+// import TimeService from '../../../services/TimeService'
 import TaskService from '../../../services/TaskService'
-import TaskDBC from '../../../dbc/TaskDBC'
-import StripePaymentIntentService from '../../../services/stripe/StripePaymentIntentService'
-import NotificationDBC from '../../../dbc/NotificationDBC';
+// import TaskDBC from '../../../dbc/TaskDBC'
+// import StripePaymentIntentService from '../../../services/stripe/StripePaymentIntentService'
+// import NotificationDBC from '../../../dbc/NotificationDBC';
 
 
 export default class SlotStatusHandler {
-  before: SlotDBC | undefined
-  after: SlotDBC | undefined
+  slot: SlotDBC
 
-  public async run(change: Change<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>) {
-    this.before = this.toDbc(change.before)
-    this.after = this.toDbc(change.after)
-    const bStatus = this.before.status
-    const aStatus = this.after.status
-    if (bStatus === 'published' && aStatus === 'holding') {
-      this.onHolding()
-    }
-    else if (bStatus === 'holding' && aStatus === 'booked') {
-      this.onPurchase()
-    }
-    else if (bStatus === 'booked' && aStatus === 'active') {
-      this.onActive()
-    }
-    else if (bStatus === 'booked' && aStatus === 'cancelled') {
-      this.onCancel()
-    }
-    else if (bStatus === 'active' && aStatus === 'disputed') {
-      this.onDispute()
+  constructor(slot: SlotDBC) {
+    this.slot = slot
+  }
+
+  public async published(): Promise<void> {
+    if (this.slot.status === 'holding') {
+      await new TaskService().onCheckout(this.slot.id!)
     }
   }
 
-  private toDbc(snapshot: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>) {
-    const data = snapshot.data()
-    return new SlotDBC(
-      snapshot.id,
-      data.name,
-      data.slot,
-      data.slots,
-      data.mandatoryFill,
-      data.start,
-      data.end,
-      data.sellerUid,
-      data.serviceDocId,
-      data.buyerUid,
-      data.buyerUsername,
-      data.paymentIntent,
-      data.status,
-      data.parentSession,
-      snapshot.ref
-    )
+  public async holding(): Promise<void> {
+    if (this.slot.status === 'booked') {
+      await new SessionDBC().increment(this.slot.parentSession!)
+      await new ChatRoomDBC().addToRoom(this.slot.buyerUid!, this.slot.parentSession!)
+    }
   }
 
-  private async onHolding(): Promise<void> {
-    await new TaskService().onCheckout(this.after!.id!)
+  public async booked(): Promise<void> {
+    switch (this.slot.status) {
+      case 'active':
+        break
+      case 'cancelled':
+        break
+    }
   }
 
-  private async onPurchase(): Promise<void> {
-    const a = this.after!
-    await new SessionDBC().increment(a.parentSession!)
-    await new ChatRoomDBC().addToRoom(a.buyerUid!, a.parentSession!)
-    a.mandatoryFill ? await this.checkFill() : await this.scheduleSlotStart()
+  public async active(): Promise<void> {
+    switch (this.slot.status) {
+      case 'succeeded':
+        break
+      case 'disputed':
+        break
+    }
+  }
+
+  public async cancelled(): Promise<void> {
+    if (this.slot.status === 'published') {
+      return
+    }
+  }
+
+  public async disputed(): Promise<void> {
+    switch (this.slot.status) {
+      case 'resolved_pos':
+        break
+      case 'resolved_neg':
+        break
+    }
+  }
+
+  //private async onPurchase(): Promise<void> {
+  //  const a = this.after!
+  //  await new SessionDBC().increment(a.parentSession!)
+  //  await new ChatRoomDBC().addToRoom(a.buyerUid!, a.parentSession!)
+  //  a.mandatoryFill ? await this.checkFill() : await this.scheduleSlotStart()
     
-    // Here we need to be sending notification and potentially mail etc etc:
-    //
-    // const creator = await new CreatorDBC(a.sellerUid).fetchByUid()
-    // const dateTime = new TimeService().toLocalDateTime(a.start!, creator.timezone!)
-    // return await new MailService(creator.email!).slotSold({
-    //   username: creator.username!,
-    //   service: a.name!,
-    //   buyer: a.buyerUsername!,
-    //   time: dateTime.time,
-    //   date: dateTime.date
-    // })
-    await new NotificationDBC(a.sellerUid!,"Session Purchased!","A user has purchased a session with you, please check your dashboard",true).send()
-  }
+  //  await new NotificationDBC(a.sellerUid!,"Session Purchased!","A user has purchased a session with you, please check your dashboard",true).send()
+  //}
 
-  private async checkFill(): Promise<void> {
-    const id = this.after!.parentSession!
-    const session = await new SessionDBC().fetch(id)
-    if (session.booked === session.slots) {
-      await session.update({ status: 'full' })
-    }
-  }
+  //private async checkFill(): Promise<void> {
+  //  const id = this.after!.parentSession!
+  //  const session = await new SessionDBC().fetch(id)
+  //  if (session.booked === session.slots) {
+  //    await session.update({ status: 'full' })
+  //  }
+  //}
 
-  private async scheduleSlotStart(): Promise<void> {
-    const a = this.after!
-    const secondsUntil = a.start! - new TimeService().generate()
-    const task = await new TaskService().scheduleSlotStart(a.id!, secondsUntil)
-    await new TaskDBC(a.id!).write(task)
-  }
+  //private async scheduleSlotStart(): Promise<void> {
+  //  const a = this.after!
+  //  const secondsUntil = a.start! - new TimeService().generate()
+  //  const task = await new TaskService().scheduleSlotStart(a.id!, secondsUntil)
+  //  await new TaskDBC(a.id!).write(task)
+  //}
 
-  private async onActive(): Promise<void> {
-    const a = this.after!
-    const secondsUntil = 10800 + a.end!
-    const task = await new TaskService().scheduleCapture(a.id!, secondsUntil)
-    await new TaskDBC(a.id!).write(task)
-  }
+  //private async onActive(): Promise<void> {
+  //  const a = this.after!
+  //  const secondsUntil = 10800 + a.end!
+  //  const task = await new TaskService().scheduleCapture(a.id!, secondsUntil)
+  //  await new TaskDBC(a.id!).write(task)
+  //}
 
-  private async onCancel(): Promise<void> {
-    const a = this.after!
-    await new SessionDBC().decrement(a.parentSession!)
-    await new ChatRoomDBC().removeFromRoom(a.buyerUid!, a.parentSession!)
-    await new StripePaymentIntentService().cancel(a.paymentIntent!)
-    a.mandatoryFill ? await new SessionDBC().onSlotCancel(a.parentSession!) : this.cancelSlotTask(a.id!)
-    await a.republish()
-  }
+  //private async onCancel(): Promise<void> {
+  //  const a = this.after!
+  //  await new SessionDBC().decrement(a.parentSession!)
+  //  await new ChatRoomDBC().removeFromRoom(a.buyerUid!, a.parentSession!)
+  //  await new StripePaymentIntentService().cancel(a.paymentIntent!)
+  //  a.mandatoryFill ? await new SessionDBC().onSlotCancel(a.parentSession!) : this.cancelSlotTask(a.id!)
+  //  await a.republish()
+  //}
 
-  private async cancelSlotTask(slotId: string): Promise<void> {
-    const task = await new TaskDBC(slotId).retrieve()
-    await new TaskService().cancel(task)
-  }
+  //private async cancelSlotTask(slotId: string): Promise<void> {
+  //  const task = await new TaskDBC(slotId).retrieve()
+  //  await new TaskService().cancel(task)
+  //}
 
-  private async onDispute(): Promise<void> {
-    console.log('disputed')
-  }
+  //private async onDispute(): Promise<void> {
+  //  console.log('disputed')
+  //}
 }
--- functions/src/endpoint/triggers/slot-status/onSlotUpdate.ts
+++ functions/src/endpoint/triggers/slot-status/onSlotUpdate.ts
@@ -1,9 +1,44 @@
 import * as functions from 'firebase-functions'
+import SlotDBC from '../../../dbc/SlotDBC'
 import SlotStatusHandler from './SlotStatusHandler'
 
 export const onSlotUpdate = functions
   .firestore
   .document('sessions/{sessionId}/slots/{slotId}')
   .onUpdate(async (change, context) => {
-    await new SlotStatusHandler().run(change)
+    const b = change.before.get('status')
+    const _ = change.after.data()
+    if (b === _.status) return
+    const slot = new SlotDBC(
+      _.id,
+      _.name,
+      _.slot,
+      _.slots,
+      _.mandatoryFill,
+      _.start,
+      _.end,
+      _.sellerUid,
+      _.serviceDocId,
+      _.buyerUid,
+      _.buyerUsername,
+      _.paymentIntent,
+      _.status,
+      _.parentSession,
+      change.after.ref
+    )
+    const handler = new SlotStatusHandler(slot)
+    switch (b) {
+      case 'published':
+        await handler.published()
+      case 'holding':
+        await handler.holding()
+      case 'booked':
+        await handler.booked()
+      case 'cancelled':
+        await handler.cancelled()
+      case 'active':
+        await handler.active()
+      case 'disputed':
+        await handler.disputed()
+    }
   })
