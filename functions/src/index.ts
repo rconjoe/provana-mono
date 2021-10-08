01b255bacaa41711316bbbea97356924adcf840e
Joe Carlton <joe@rconjoe.com>
2021-10-08 17:28:19 -0400

    export cancelSlot and cancelSessionhttpscallable endpoints

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,3 @@
-import * as functions from "firebase-functions";
-import { db } from './config'
-
-export const testFunc = functions.https.onCall(async (data, context) => {
-  const uid = context.auth && context.auth.uid
-  const message = data.message
-  const write: FirebaseFirestore.DocumentReference =
-    await db.collection('test-messages').add({message: message})
-
-  return `${uid} said ${message}, and I wrote it to firestore at messages/${write.id}`
-})
-
 export { registerSupporter } from './endpoint/onCall/registerSupporter'
 export { registerCreator } from './endpoint/onCall/registerCreator'
 export { getOrCreateInvitation } from './endpoint/onCall/getOrCreateInvitation'
@@ -26,5 +14,7 @@ export { publishPotential } from './endpoint/onCall/publishPotential'
 export { checkout } from './endpoint/onCall/checkout'
 export { confirmCheckoutComplete } from './endpoint/onRequest/confirmCheckoutComplete'
 export { checkoutComplete } from './endpoint/onCall/checkoutComplete'
+export { cancelSlot } from './endpoint/onCall/cancelSlot'
+export { cancelSession } from './endpoint/onCall/cancelSession'
 export { startSlot } from './endpoint/onRequest/startSlot'
 export { capture } from './endpoint/onRequest/capture'
