8dce44f12c4500630fc9dbc2dcb533981d4166fe
dustingits <dustinderouen@me.com>
2021-10-20 11:43:10 -0500

    new alpha partner alert

---
+++ vue2/src/components/Nav/AlphaPartnerAlert.vue
@@ -0,0 +1,35 @@
+<template>
+	<v-alert dense dismissible id="alphaAlert" color="primary" >
+		<h1 class="alphaAlertText">we're looking for alpha partners to give feedback! </h1>
+		<v-btn dense class="btnCTA" id="discordBtn" href="https://discord.gg/zJh4RaHk" target="_blank" color="#5865F2">
+			join our discord
+		</v-btn>
+	</v-alert>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style scoped>
+	#discordBtn {
+		color: white;
+		font: normal 600 .85vw Arboria;
+        height: 1.5vw;
+	}
+	#discordBtn:hover {
+		color: #5865f2;
+	}
+	.alphaAlertText {
+		vertical-align: middle;
+		font: normal 600 .85vw Arboria;
+		letter-spacing: -0.005125vw;
+		text-transform: none;
+		display: inline-block;
+		margin-right: 2vw;
+	}
+	#alphaAlert {
+		position: ;
+		margin-top: 60px;
+	}
+</style>
