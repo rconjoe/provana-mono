2b4d95c02c00bcf8cad1f67534d3c8c89ceab7d6
Joe Carlton <joe@rconjoe.com>
2021-10-21 04:33:45 -0400

    init disputedOfSeller fetching method on SlotDBC. returns all disputed slots of a given partner's UID

--- functions/src/dbc/SlotDBC.ts
+++ functions/src/dbc/SlotDBC.ts
@@ -288,4 +288,25 @@ export default class SlotDBC extends Slot {
     return slots
   }
 
+  public async disputedOfSeller(uid: string): Promise<Array<Slot>> {
+    let slots: Array<Slot> = []
+    let q = await db.collectionGroup('slots')
+      .where('sellerUid', '==', uid)
+      .where('status', '==', 'disputed')
+      .withConverter(converter)
+      .get()
+    if (q.size > 0) {
+      q.forEach(slot => {
+        slots.push(slot.data().toModel())
+      })
+    }
+    return slots
+  }
+
+  public async resolve(by: string): Promise<void> {
+    if (this.id === "" || this.id === undefined) throw new Error('Pass slot ID in constructor to run resolve(by)')
+    let slot = await this.fromId(this.id!)
+    await slot.update({ status: 'resolved-seller' })
+  }
+
 }
