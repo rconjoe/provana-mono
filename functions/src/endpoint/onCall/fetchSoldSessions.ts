930be3d67f110da9db45a3245e3f184a39473f45
Joe Carlton <joe@rconjoe.com>
2021-10-23 18:53:08 -0400

    replace fetchSoldSessions with fetchSoldSlot endpoint

--- functions/src/endpoint/onCall/fetchSoldSessions.ts
+++
@@ -1,6 +0,0 @@
-import * as functions from 'firebase-functions'
-import SessionDBC from '../../dbc/SessionDBC'
-
-export const fetchSoldSessions = functions.https.onCall(async (data, context) => {
-  return await new SessionDBC().fetchBooked(data.sellerUid)
-})
