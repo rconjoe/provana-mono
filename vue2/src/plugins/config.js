71af2519bce3bf47c4da40f6cc1bc8b668dd12eb
Joe Carlton <joe@rconjoe.com>
2021-10-24 18:00:20 -0400

    new firebase projects on provana.gg accounts

--- vue2/src/plugins/config.js
+++ vue2/src/plugins/config.js
@@ -1,11 +1,11 @@
 export const config =  {
-  apiKey: "AIzaSyBBBrVHVYBo4Hrc3rtsdOaCjN33R-9ypjY",
-  authDomain: "db-abstract.firebaseapp.com",
-  projectId: "db-abstract",
-  storageBucket: "db-abstract.appspot.com",
-  messagingSenderId: "902848736944",
-  appId: "1:902848736944:web:49998bcb0ba26d4001d575",
-  measurementId: "G-MQKBZ5H9HT"
+  apiKey: "AIzaSyBWXyt6r4-dRqddIWt7PIRC7M3D3O-u6D4",
+  authDomain: "pv-dev-4e2c2.firebaseapp.com",
+  projectId: "pv-dev-4e2c2",
+  storageBucket: "pv-dev-4e2c2.appspot.com",
+  messagingSenderId: "17716284907",
+  appId: "1:17716284907:web:72b1b0ce4b25e90ff01dae",
+  measurementId: "G-K1R3DSZR6H"
 }
 
 export const spub = 'pk_test_51HJUgfGoIl5NLNcQKTXPu3CKuckXq6vbUXxASrRZvrXgwtODSI9wFNWdZoo37LY3YXrrfMx2N7Nas1MWbWn7ddu100RWAa63mC'
