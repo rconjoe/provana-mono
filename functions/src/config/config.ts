f485e405cd278728e43e3b4e1ec74f95a1b6b55b
Joe Carlton <joe@rconjoe.com>
2021-10-24 16:53:53 -0400

    update config file for emulator usage downstream

--- functions/src/config/config.ts
+++ functions/src/config/config.ts
@@ -1,17 +1,22 @@
-import * as functions from 'firebase-functions'
 import Stripe from 'stripe'
 import * as sendgrid from '@sendgrid/mail'
-import { ClientOptions } from '@grpc/grpc-js'
+import { ClientOptions, credentials } from '@grpc/grpc-js'
 
-export var stripe = new Stripe(
-  functions.config().stripe.live_key, {
-    apiVersion: '2020-08-27'
+export var stripe = new Stripe(  "sk_test_51HJUgfGoIl5NLNcQ8xzPwo3tXqwoaGym8ZXwPBxbVWuOEEdCQxst4ORTV9x8GU4k4TK9uyFFiB9zLHMvDMLSV9UW00N8C4ejVB", {
+    apiVersion: '2020-08-27',
+    host: 'localhost',
+    protocol: 'http',
+    port: '12111'
   }
 )
 
 export var stripeRedirectUrl = 'http://localhost:8081'
 
-sendgrid.setApiKey(functions.config().sendgrid.live_key)
+sendgrid.setApiKey("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")
 export const sg = sendgrid
 
-export var taskcfg: ClientOptions = {}
+export var taskcfg: ClientOptions = {
+  port: 8001,
+  servicePath: 'localhost',
+  sslCreds: credentials.createInsecure()
+}
