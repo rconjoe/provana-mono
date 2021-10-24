671a6d5ac50b8e5cb114a7ead5b72edebd262bda
Joe Carlton <joe@rconjoe.com>
2021-10-24 17:02:22 -0400

    config edits that should be preserved when this branch is merged into

--- functions/src/config/config.ts
+++ functions/src/config/config.ts
@@ -1,22 +1,17 @@
+import * as functions from 'firebase-functions'
 import Stripe from 'stripe'
 import * as sendgrid from '@sendgrid/mail'
-import { ClientOptions, credentials } from '@grpc/grpc-js'
+import { ClientOptions } from '@grpc/grpc-js'
 
-export var stripe = new Stripe(  "sk_test_51HJUgfGoIl5NLNcQ8xzPwo3tXqwoaGym8ZXwPBxbVWuOEEdCQxst4ORTV9x8GU4k4TK9uyFFiB9zLHMvDMLSV9UW00N8C4ejVB", {
-    apiVersion: '2020-08-27',
-    host: 'localhost',
-    protocol: 'http',
-    port: '12111'
+export var stripe = new Stripe(
+  functions.config().stripe.test_key, {
+    apiVersion: '2020-08-27'
   }
 )
 
 export var stripeRedirectUrl = 'http://localhost:8081'
 
-sendgrid.setApiKey("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")
+sendgrid.setApiKey(functions.config().sendgrid.live_key)
 export const sg = sendgrid
 
-export var taskcfg: ClientOptions = {
-  port: 8001,
-  servicePath: 'localhost',
-  sslCreds: credentials.createInsecure()
-}
+export var taskcfg: ClientOptions = {}
