6cd92cd0496611022623f23a47710ee6bf8ecf15
dustingits <dustinderouen@me.com>
2021-11-05 10:10:38 -0500

    global styles added for button font minimum

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -1,38 +1,37 @@
 <template>
-  <v-app>
-    <!-- global navbar -->
+	<v-app>
+		<!-- global navbar -->
 
-   
+		<!-- router content -->
+		<v-main>
+			<v-container fluid>
+				<CustomAppBar />
 
-    <!-- router content -->
-    <v-main>
-      <v-container fluid>
-         <CustomAppBar />
-        <v-progress-linear
-          color="grey lighten-5"
-          background-color="grey darken-1"
-          rounded
-          indeterminate
-          :active="loading"
-        >
-        </v-progress-linear>
-        <v-alert
-          class="errorMessage"
-          dismissible
-          id="error"
-          type="warning"
-          :icon="error.icon"
-          :color="error.color"
-          v-model="error.show"
-        >
-          {{error.message}}
-        </v-alert>
-        <router-view />
-      </v-container>
-    </v-main>
-    <!-- footer -->
-    <Footer />
-  </v-app>
+				<v-progress-linear
+					color="grey lighten-5"
+					background-color="grey darken-1"
+					rounded
+					indeterminate
+					:active="loading"
+				>
+				</v-progress-linear>
+				<v-alert
+					class="errorMessage"
+					dismissible
+					id="error"
+					type="warning"
+					:icon="error.icon"
+					:color="error.color"
+					v-model="error.show"
+				>
+					{{ error.message }}
+				</v-alert>
+				<router-view />
+			</v-container>
+		</v-main>
+		<!-- footer -->
+		<Footer />
+	</v-app>
 </template>
 
 <script>
@@ -41,86 +40,88 @@ import Footer from '@/components/Nav/Footer.vue'
 import { mapState } from 'vuex'
 
 export default {
-  name: 'App',
-  appNavDrawer: false,
-  components: {CustomAppBar, Footer},
-  data: () => ({
-    scrollTop: 1,
-  }),
-  computed: mapState({
-    error: state => state.error.data,
-    loading: state => state.loading.show
-  }),
-};
+	name: 'App',
+	appNavDrawer: false,
+	components: { CustomAppBar, Footer },
+	data: () => ({
+		scrollTop: 1,
+	}),
+	computed: mapState({
+		error: (state) => state.error.data,
+		loading: (state) => state.loading.show,
+	}),
+}
 </script>
-<style>
+<style lang="scss">
 /* scroll bar theme */
 :root {
-  color-scheme: dark;
+	color-scheme: dark;
 }
 html {
-  --scrollbarBG: #111;
-  --thumbBG: #90A4AE;
+	--scrollbarBG: #111;
+	--thumbBG: #90a4ae;
 }
 ::-webkit-scrollbar {
-  		width: 0.52vw;
-		padding-left: 1.0417vw;
 }
 body {
-  scrollbar-width: thin;
-  scrollbar-color: #111;
+	scrollbar-width: thin;
+	scrollbar-color: #111;
 }
 ::-webkit-scrollbar-corner {
-  background: rgba(0,0,0,0);
+	background: rgba(0, 0, 0, 0);
 }
 ::-webkit-scrollbar-track {
-  background: #111111;
+	background: #111111;
 }
 ::-webkit-scrollbar-thumb {
- 		background: #333333;
-		border-radius: 7px;
+	background: #333333;
+	border-radius: 7px;
 }
-
+.icon {
+	font-size: 50px;
+}
 .v-alert__content {
-  text-transform: uppercase;
+	text-transform: uppercase;
 }
 #error {
-  opacity: 1;
-  z-index:8;
-  position:fixed !important;
-  width: 99%;
+	opacity: 1;
+	z-index: 8;
+	position: fixed !important;
+	width: 99%;
+	font: normal 600 1rem Arboria;
+	text-transform: lowercase;
 }
 
 html {
-  scroll-behavior: smooth;
+	scroll-behavior: smooth;
 }
 /* Global btn style */
 
-.btnCTA.v-btn{
-  min-width:2.6041666666666665vw;
-  font: normal 700 0.78215vw Poppins;
-		letter-spacing: 0px;
-		border-radius: 8px;
+.btnCTA.v-btn {
+	font: normal 700 1.2 Poppins;
+	letter-spacing: 0px;
+	padding: 0 24px;
+	border-radius: 10px;
 }
-  .btnCTA:hover {
-		color: #fa4b6b ;
-	}
-  .btnCTA.v-btn:hover::before{
-		color: #f5f5f5;
-		opacity: 1;
-	}
-  .btnCTA.v-btn:hover{
-		color: #fa4b6b;
-    background-color: #f5f5f5  !important;;
-  }
+.btnCTA:hover {
+	color: #fa4b6b;
+}
+.btnCTA.v-btn:hover::before {
+	color: #f5f5f5;
+	opacity: 1;
+}
+.btnCTA.v-btn:hover {
+	color: #fa4b6b;
+	background-color: #f5f5f5 !important;
+}
 input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
+	-webkit-appearance: none;
+	margin: 0;
 }
 
 /* Firefox */
-input[type=number] {
-  -moz-appearance: textfield;
+input[type='number'] {
+	-moz-appearance: textfield;
 }
 </style>
