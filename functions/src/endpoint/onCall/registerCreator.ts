1d53d69bd52933d92c77da1e9250de940e8eba8a
Joe Carlton <joe@rconjoe.com>
2021-11-09 09:56:39 -0800

    init calls that check username availablity. trying to get rid of these w lodash in frontend instead?

--- functions/src/endpoint/onCall/registerCreator.ts
+++ functions/src/endpoint/onCall/registerCreator.ts
@@ -4,9 +4,14 @@ import AuthUserService from '../../services/auth/AuthUserService'
 import StripeCustomerService from '../../services/stripe/StripeCustomerService'
 import StripeAccountService from '../../services/stripe/StripeAccountService'
 import CreatorDBC from '../../dbc/CreatorDBC'
+import UserFinder from '../../services/UserFinder'
 
-export const registerCreator = functions.https.onCall(async (data, context) => {
+export const registerCreator = functions.https.onCall(async (data, context): 
+  Promise<'USERNAME_TAKEN'|'OK'> => {
   if (data === null || data === undefined) throw new Error('Null payload!')
+
+  let usernameAvailable = await new UserFinder().usernameAvailable(data.username)
+  if (usernameAvailable === false) return 'USERNAME_TAKEN'
 
   const creator = new Creator().setRegisterData(data.email, data.password, data.code, data.username)
   await new AuthUserService().registerCreator(creator)
@@ -15,5 +20,6 @@ export const registerCreator = functions.https.onCall(async (data, context) => {
   await new StripeAccountService().create(creator)
 
   await new CreatorDBC().writeNew(creator)
-  return 'ok'
+  return 'OK'
 })
+
--- functions/src/endpoint/onCall/registerSupporter.ts
+++ functions/src/endpoint/onCall/registerSupporter.ts
@@ -4,9 +4,14 @@ import AuthUserService from '../../services/auth/AuthUserService'
 import StripeCustomerService from '../../services/stripe/StripeCustomerService'
 import SupporterDBC from '../../dbc/SupporterDBC'
 import DiscordLink from '../../models/DiscordLink'
+import UserFinder from '../../services/UserFinder'
 
-export const registerSupporter = functions.https.onCall(async (data, context) => {
+export const registerSupporter = functions.https.onCall(async (data, context):
+  Promise<'USERNAME_TAKEN'|'OK'> => {
   if (data === null || data === undefined) throw new Error('Null payload!')
+
+  let usernameAvailable = await new UserFinder().usernameAvailable(data.username)
+  if (usernameAvailable === false) return 'USERNAME_TAKEN'
  
   // This is a stinky, dirty, silly hack to get quick discord integration in before launch.
   const discord = new DiscordLink().generate()
@@ -15,5 +20,5 @@ export const registerSupporter = functions.https.onCall(async (data, context) =>
   await new AuthUserService().registerSupporter(supporter)
   await new StripeCustomerService().newSupporter(supporter)
   await new SupporterDBC().writeNew(supporter)
-  return 'ok'
+  return 'OK'
 })
