e94edf751670dda213a2148c07bac304a72b20bb
Joe Carlton <joe@rconjoe.com>
2021-11-10 09:24:47 -0800

    on supporter register, store duplicate username and vanity

--- functions/src/endpoint/onCall/registerSupporter.ts
+++ functions/src/endpoint/onCall/registerSupporter.ts
@@ -4,14 +4,10 @@ import AuthUserService from '../../services/auth/AuthUserService'
 import StripeCustomerService from '../../services/stripe/StripeCustomerService'
 import SupporterDBC from '../../dbc/SupporterDBC'
 import DiscordLink from '../../models/DiscordLink'
-import UserFinder from '../../services/UserFinder'
 
 export const registerSupporter = functions.https.onCall(async (data, context):
-  Promise<'USERNAME_TAKEN'|'OK'> => {
+  Promise<void> => {
   if (data === null || data === undefined) throw new Error('Null payload!')
-
-  let usernameAvailable = await new UserFinder().usernameAvailable(data.username)
-  if (usernameAvailable === false) return 'USERNAME_TAKEN'
  
   // This is a stinky, dirty, silly hack to get quick discord integration in before launch.
   const discord = new DiscordLink().generate()
@@ -20,5 +16,4 @@ export const registerSupporter = functions.https.onCall(async (data, context):
   await new AuthUserService().registerSupporter(supporter)
   await new StripeCustomerService().newSupporter(supporter)
   await new SupporterDBC().writeNew(supporter)
-  return 'OK'
 })
