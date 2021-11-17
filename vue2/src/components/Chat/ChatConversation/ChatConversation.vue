afef5f27b318d3b9639ab2efcc4bc1e046c1b77a
dustingits <dustinderouen@me.com>
2021-11-16 20:39:03 -0600

    chat responsive update

--- vue2/src/components/Chat/ChatConversation/ChatConversation.vue
+++ vue2/src/components/Chat/ChatConversation/ChatConversation.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-card max-width="380" min-height="600" class="ma-0 pa-0">
+	<v-card class="ma-0 pa-0 chatContainer">
 		<!-- Toolbar header  with Username and order details -->
 		<ConvoToolbar
 			:title="chatroom.title"
@@ -10,111 +10,111 @@
 			@back-to-rooms="backToRooms"
 		/>
 		<!-- Chat Conversation -->
-		<Messages :uid="uid" :messages="messages" />
-		<v-row>
-			<v-col class="pb-0">
-				<SendMessage @send-message="sendMessage" />
-			</v-col>
-		</v-row>
+		<Messages class="messages" :uid="uid" :messages="messages" />
+		<SendMessage @send-message="sendMessage" />
 	</v-card>
 </template>
 
 <script>
-	import { mapState } from 'vuex'
-	import { db } from '../../../plugins/firebase'
-	import dayjs from 'dayjs'
-	import utc from 'dayjs/plugin/utc'
-	import timezone from 'dayjs/plugin/timezone'
-	import SendMessage from './SendMessage.vue'
-	import ConvoToolbar from './ConvoToolbar.vue'
-	import Messages from './Messages.vue'
-	dayjs.extend(utc)
-	dayjs.extend(timezone)
+import { mapState } from 'vuex'
+import { db } from '../../../plugins/firebase'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import SendMessage from './SendMessage.vue'
+import ConvoToolbar from './ConvoToolbar.vue'
+import Messages from './Messages.vue'
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
-	export default {
-		name: 'ChatConvo',
-		props: ['chatroom'],
-		components: { SendMessage, ConvoToolbar, Messages },
+export default {
+	name: 'ChatConvo',
+	props: ['chatroom'],
+	components: { SendMessage, ConvoToolbar, Messages },
 
-		data: () => ({
-			editTitle: false,
-			messages: [],
-			users: [],
-		}),
-		computed: mapState({
-			profile: (state) => state.auth.currentUser,
-		}),
-		mounted() {
-			this.uid = this.$user.uid
-			const messages = db
+	data: () => ({
+		editTitle: false,
+		messages: [],
+		users: [],
+	}),
+	computed: mapState({
+		profile: (state) => state.auth.currentUser,
+	}),
+	mounted() {
+		this.uid = this.$user.uid
+		const messages = db
+			.collection('chats')
+			.doc(this.chatroom.roomId)
+			.collection('messages')
+		messages.onSnapshot(async (snapshot) => {
+			this.messages = []
+			let temp = []
+			snapshot.forEach((message) => {
+				const data = message.data()
+				const tz = this.$store.state.auth.tz
+				const unixAt = data.at
+				const _at = dayjs.unix(unixAt).format()
+				const at = dayjs(_at)
+					.tz(tz)
+					.format('hh:mm')
+				const msg = {
+					from: data.from,
+					username: data.username,
+					at: at,
+					unixAt: unixAt,
+					text: data.text,
+					room: this.chatroom.id,
+					avatar: data.avatar,
+				}
+				temp.push(msg)
+			})
+			temp.sort(function compareTimes(a, b) {
+				return a.unixAt - b.unixAt
+			})
+			this.messages = temp
+		})
+	},
+	methods: {
+		async sendMessage(message) {
+			await db
 				.collection('chats')
 				.doc(this.chatroom.roomId)
 				.collection('messages')
-			messages.onSnapshot(async (snapshot) => {
-				this.messages = []
-				let temp = []
-				snapshot.forEach((message) => {
-					const data = message.data()
-					const tz = this.$store.state.auth.tz
-					const unixAt = data.at
-					const _at = dayjs.unix(unixAt).format()
-					const at = dayjs(_at)
-						.tz(tz)
-						.format('hh:mm')
-					const msg = {
-						from: data.from,
-						username: data.username,
-						at: at,
-						unixAt: unixAt,
-						text: data.text,
-						room: this.chatroom.id,
-						avatar: data.avatar,
-					}
-					temp.push(msg)
+				.add({
+					from: this.profile.uid,
+					username: this.profile.username,
+					at: dayjs().unix(),
+					text: `${message}`,
+					room: this.chatroom.roomId,
+					avatar: this.profile.avatar,
 				})
-				temp.sort(function compareTimes(a, b) {
-					return a.unixAt - b.unixAt
+				.catch((err) => {
+					console.error(`Error sending that message: ${err}`)
 				})
-				this.messages = temp
-			})
 		},
-		methods: {
-			async sendMessage(message) {
-				await db
-					.collection('chats')
-					.doc(this.chatroom.roomId)
-					.collection('messages')
-					.add({
-						from: this.profile.uid,
-						username: this.profile.username,
-						at: dayjs().unix(),
-						text: `${message}`,
-						room: this.chatroom.roomId,
-						avatar: this.profile.avatar,
-					})
-					.catch((err) => {
-						console.error(`Error sending that message: ${err}`)
-					})
-			},
-			toggleEdit() {
-				this.editTitle = !this.editTitle
-			},
-			async changeTitle(t) {
-				await db
-					.collection('chats')
-					.doc(this.chatroom.roomId)
-					.update({ title: t })
-					.catch((err) => {
-						console.error(`Error updating title: ${err}`)
-					})
-				this.backToRooms()
-			},
+		toggleEdit() {
+			this.editTitle = !this.editTitle
+		},
+		async changeTitle(t) {
+			await db
+				.collection('chats')
+				.doc(this.chatroom.roomId)
+				.update({ title: t })
+				.catch((err) => {
+					console.error(`Error updating title: ${err}`)
+				})
+			this.backToRooms()
+		},
 
-			backToRooms() {
-				this.$emit('back-to-rooms')
-			},
+		backToRooms() {
+			this.$emit('back-to-rooms')
 		},
-	}
+	},
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.chatContainer {
+	height: 100%;
+}
+</style>
--- vue2/src/components/Chat/ChatConversation/SendMessage.vue
+++ vue2/src/components/Chat/ChatConversation/SendMessage.vue
@@ -1,43 +1,61 @@
 <template>
-    <!-- New message input box -->
-        <v-form @submit="sendMessage">
-        <v-textarea
-          hide-details
-          v-model='message'
-          no-resize
-          height="100"
-          filled
-          class="messageInput p-0"
-          name="input-7-4"
-          placeholder="Write a reply..."
-        >
-        <v-icon @click="sendMessage(message)" class="sendBtn" slot="append" :disabled="message == '' "> fas fa-paper-plane</v-icon>
-        </v-textarea>
-        </v-form>
+	<!-- New message input box -->
+	<v-form @submit="sendMessage" class="messageInput">
+		<v-textarea
+			hide-details
+			v-model="message"
+			no-resize
+			height="100"
+			filled
+			id="sendMessage"
+			class="p-0"
+			name="input-7-4"
+			placeholder="Write a reply..."
+		>
+			<v-icon @click="sendMessage(message)" class="sendBtn" slot="append" :disabled="message == ''">
+				fas fa-paper-plane</v-icon
+			>
+		</v-textarea>
+	</v-form>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        message:''
-        
-    }),
-    methods:{
-        sendMessage(m){
-            this.$emit('send-message',m)
-            this.message = ''
-        }
-    }
+	data: () => ({
+		message: '',
+	}),
+	methods: {
+		sendMessage(m) {
+			this.$emit('send-message', m)
+			this.message = ''
+		},
+	},
 }
 </script>
 
 <style scoped>
-.sendBtn {
-  margin-top: 50px;
-  margin-right: 5px;
+/* v-text area  */
+::v-deep .theme--dark.v-text-field--filled > .v-input__control > .v-input__slot {
+	background: #5c5b5b;
 }
-.sendBtn:hover{
-  color:#D4145A;
-  cursor: pointer;
+/* v-text area hover */
+::v-deep
+	.theme--dark.v-text-field--filled:not(.v-input--is-focused):not(.v-input--has-state)
+	> .v-input__control
+	> .v-input__slot:hover {
+	background: #666666;
+}
+.messageInput {
+	position: sticky;
+	bottom: 0;
+	z-index: 100;
+}
+.sendBtn {
+	margin-top: 50px;
+	margin-right: 5px;
+}
+.sendBtn:hover {
+	color: #d4145a;
+	cursor: pointer;
 }
 </style>
