fbb1afb447f6faefd19648464162f36bf2da2689
dustingits <dustinderouen@me.com>
2021-11-18 23:52:28 -0600

    new tip card for dashboard pages

---
+++ vue2/src/components/DashboardComponents/TipCard.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="tipContainer">
+		<h2 class="tipHeader"> What do I do here? </h2>
+		<p class="tipText">
+			<slot> </slot>
+		</p>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.tipContainer {
+	height: 100%;
+	width: 100%;
+	background-color: #1e1e1e;
+	padding: 30px 24px;
+}
+.tipHeader {
+	font: normal 600 20px Poppins;
+	letter-spacing: -1px;
+	color: #fa4b6b;
+	text-align: center;
+	padding-bottom: 15px;
+}
+.tipText {
+	font: normal 500 15px Arboria;
+}
+</style>
