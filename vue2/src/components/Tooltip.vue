d7820ff758f9e9762d988fe5335397bc2e6f1a08
dustingits <dustinderouen@me.com>
2021-11-25 01:01:10 -0600

    NEW tooltip reusable tooltip component

---
+++ vue2/src/components/Tooltip.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-tooltip right color="#333333">
+		<template v-slot:activator="{ on, attrs }">
+			<v-icon size="15px" color="#333333" class="mr-2 align-self-center" v-on="on" v-bind="attrs"
+				>fas fa-exclamation-circle
+			</v-icon>
+		</template>
+		<span id="toolTip">
+			<slot> </slot>
+		</span>
+	</v-tooltip>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+#toolTip {
+	text-align: center;
+	max-width: 270px;
+}
+</style>
