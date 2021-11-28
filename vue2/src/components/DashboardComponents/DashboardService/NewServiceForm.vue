ea374aeccb9fa99faf98e3b3373cc2cb339a47ed
dustingits <dustinderouen@me.com>
2021-11-28 17:18:28 -0600

    form when creating a new service responsive updated

--- vue2/src/components/DashboardComponents/DashboardService/NewServiceForm.vue
+++ vue2/src/components/DashboardComponents/DashboardService/NewServiceForm.vue
@@ -1,263 +1,271 @@
 <template>
-	<div class="newServiceContainer" ref="formBox">
+	<div class="newServiceContainer " ref="formBox">
 		<div class="d-flex justify-center my-auto fill-height" v-if="!showForm" @click="animateForm">
 			<v-icon color="#ED2970" class="mr-1" size="14px"> fas fa-plus </v-icon>
 			<h2 class="newServiceBtn"> New Service </h2>
 		</div>
-		<div v-else>
-			<v-form ref="serviceForm" @submit.prevent="createService" v-model="serviceValid">
-				<div class="fieldsContainer">
-					<!-- Header -->
-					<h1> New Service </h1>
-					<!-- Title -->
-					<div class="field">
-						<v-text-field v-model="form.serviceName" :rules="nameRules">
-							<span class="titleLabel" slot="prepend"> Title: </span>
-						</v-text-field>
-					</div>
 
-					<!-- Image upload -->
-					<div class="field">
-						<div class="pictureTip">
-							<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
+		<div v-show="showForm" class="fill-height">
+			<v-form ref="serviceForm" class="fieldsContainer" @submit.prevent="createService" v-model="serviceValid">
+				<!-- Header -->
+				<h1 class="field"> New Service </h1>
+				<!-- Title -->
+				<div class="field" :key="1">
+					<v-text-field v-model="form.serviceName" :rules="nameRules" hide-details class="pt-0">
+						<span class="titleLabel" slot="prepend"> Title: </span>
+					</v-text-field>
+				</div>
+
+				<!-- Image upload -->
+				<div class="field" :key="2">
+					<div class="pictureTip">
+						<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
+					</div>
+					<div>
+						<h3 class="label mb-4 align-self-start" slot="prepend"> Service Picture: </h3>
+						<h3 class="link" @click="pictureOverlay = !pictureOverlay"> Add </h3>
+					</div>
+					<v-img
+						class="serviceImg"
+						src="https://dummyimage.com/1920x960/1e1e1e/dbdbdb"
+						ratio="16/9"
+						max-width="174"
+					>
+					</v-img>
+					<Overlay :overlay="pictureOverlay" @close-overlay="pictureOverlay = !pictureOverlay">
+						<span slot="title"> Service Picture </span>
+						<div slot="content">
+							<v-file-input @change="processStorePic($event)" truncate-length="15"></v-file-input>
+
+							<v-btn @click="uploadStorePic" color="#fa4b6b" text :ripple="false"> Save </v-btn>
+							<v-btn @click="pictureOverlay = !pictureOverlay" color="#666666" text :ripple="false">
+								Cancel
+							</v-btn>
 						</div>
-						<div>
-							<h3 class="label mb-4 align-self-start" slot="prepend"> Service Picture: </h3>
-							<h3 class="link" @click="pictureOverlay = !pictureOverlay"> Add </h3>
-						</div>
-						<v-img
-							class="serviceImg"
-							src="https://dummyimage.com/1920x960/1e1e1e/dbdbdb"
-							ratio="16/9"
-							max-width="174"
-						>
-						</v-img>
-						<Overlay :overlay="pictureOverlay" @close-overlay="pictureOverlay = !pictureOverlay">
-							<span slot="title"> Service Picture </span>
-							<div slot="content">
-								<v-file-input @change="processStorePic($event)" truncate-length="15"></v-file-input>
+					</Overlay>
+				</div>
 
-								<v-btn @click="uploadStorePic" color="#fa4b6b" text :ripple="false"> Save </v-btn>
-								<v-btn @click="pictureOverlay = !pictureOverlay" color="#666666" text :ripple="false">
-									Cancel
-								</v-btn>
-							</div>
-						</Overlay>
-					</div>
-
-					<!-- ColorPicker -->
-					<div class="field">
-						<div>
-							<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
-						</div>
-						<span class="label align-self-start" slot="prepend"> Service Color: </span>
-						<v-color-picker
-							v-model="form.serviceColor"
-							class="colorPicker mb-2"
-							hide-inputs
-							hide-mode-switch
-							width="174px"
-							canvas-height="60"
-						>
-						</v-color-picker>
-					</div>
-
-					<!-- Price -->
-					<div class="field">
+				<!-- ColorPicker -->
+				<div class="field mt-2" :key="3">
+					<div>
 						<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
-						<span class="label"> Price: </span>
-						<v-text-field
-							data-test="servicePrice-input"
-							class="pt-1 mt-0 inputText"
-							v-model.number="form.serviceCost"
-							color="#fff"
-							type="number"
-							:rules="hoursRules"
-							required
-						>
-							<v-icon size="20px" class="mt-2" slot="prepend" color="#cccccc">
-								fas fa-dollar-sign
-							</v-icon>
-						</v-text-field>
 					</div>
+					<span class="label align-self-start" slot="prepend"> Service Color: </span>
+					<v-color-picker
+						v-model="form.serviceColor"
+						class="colorPicker mb-2"
+						hide-inputs
+						hide-mode-switch
+						width="174px"
+						canvas-height="60"
+					>
+					</v-color-picker>
+				</div>
 
-					<!-- Length -->
-					<div class="field">
+				<!-- Price -->
+				<div class="field" :key="4">
+					<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
+					<span class="label"> Price: </span>
+					<v-text-field
+						data-test="servicePrice-input"
+						class="pt-1 mt-0 inputText"
+						v-model.number="form.serviceCost"
+						color="#fff"
+						type="number"
+						hide-details
+						:rules="hoursRules"
+						required
+					>
+						<v-icon size="20px" class="mt-2" slot="prepend" color="#cccccc">
+							fas fa-dollar-sign
+						</v-icon>
+					</v-text-field>
+				</div>
+
+				<!-- Length -->
+				<div class="field" :key="5">
+					<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
+					<span class="label "> Length: </span>
+					<!-- hours -->
+					<v-select
+						class="pt-0 mt-0 mr-6 lengthInput inputText"
+						v-model="form.serviceHours"
+						color="#fff"
+						style="max-width:94px"
+						single-line
+						:items="[0, 1, 2, 3, 4]"
+					>
+						<small slot="append-outer" class="white--text small mt-4"> Hrs</small>
+					</v-select>
+					<!-- mins -->
+					<v-select
+						class="pt-0 mt-0 "
+						style="max-width:94px"
+						v-model="form.serviceMinutes"
+						color="#fff"
+						single-line
+						min="0"
+						:items="minutes"
+					>
+						<small slot="append-outer" class="white--text small mt-4"> Mins</small>
+					</v-select>
+				</div>
+
+				<!-- Buyers -->
+				<div class="field" :key="6">
+					<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
+					<span class="label"> Buyers:</span>
+					<v-text-field
+						class="pt-0 mt-0 mr-2"
+						style="max-width:55px"
+						v-model.number="form.attendees"
+						color="#fff"
+						single-line
+						hide-details
+						type="number"
+						required
+						:rules="hoursRules"
+					>
+					</v-text-field>
+					<div class="d-flex align-center ">
 						<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
-						<span class="label "> Length: </span>
-						<!-- hours -->
-						<v-select
-							class="pt-0 mt-0 mr-6 lengthInput inputText"
-							v-model="form.serviceHours"
-							color="#fff"
-							style="max-width:94px"
-							single-line
-							:items="[0, 1, 2, 3, 4]"
+						<span class="mr-2" style="color:#dbdbdb; font: normal 600 15px Arboria">Mandatory Fill?</span>
+						<v-checkbox
+							v-model="form.mandatoryFill"
+							class="checkbox mt-1 pt-0 mx-0 align-self-center"
+							hide-details
+							:ripple="false"
+							dense
 						>
-							<small slot="append-outer" class="white--text small mt-4"> Hrs</small>
-						</v-select>
-						<!-- mins -->
-						<v-select
-							class="pt-0 mt-0 "
-							style="max-width:94px"
-							v-model="form.serviceMinutes"
-							color="#fff"
-							single-line
-							min="0"
-							:items="minutes"
-						>
-							<small slot="append-outer" class="white--text small mt-4"> Mins</small>
-						</v-select>
-					</div>
-
-					<!-- Buyers -->
-					<div class="field">
-						<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
-						<span class="label"> Buyers:</span>
-						<v-text-field
-							class="pt-0 mt-0 mr-2"
-							style="max-width:55px"
-							v-model.number="form.attendees"
-							color="#fff"
-							single-line
-							type="number"
-							required
-							:rules="hoursRules"
-						>
-						</v-text-field>
-						<div class="d-flex align-center ">
-							<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
-							<span class="mr-2" style="color:#dbdbdb; font: normal 600 15px Arboria"
-								>Mandatory Fill?</span
-							>
-							<v-checkbox
-								v-model="form.mandatoryFill"
-								class="checkbox mt-1 pt-0 mx-0 align-self-center"
-								hide-details
-								:ripple="false"
-								dense
-							>
-							</v-checkbox>
-						</div>
-					</div>
-
-					<!-- Software -->
-					<div class="field">
-						<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
-						<span class="label"> Software:</span>
-						<v-text-field
-							data-test="software-input"
-							class="pt-0 mt-0 inputText"
-							v-model="form.software"
-							color="#fff"
-							single-line
-							counter="20"
-							:rules="softwareRules"
-							required
-						>
-						</v-text-field>
-					</div>
-
-					<!-- Terms -->
-					<div class="field">
-						<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
-						<span class="label"> Terms:</span>
-						<span class="link" @click="termsOverlay = !termsOverlay"> Add Terms</span>
-						<Overlay :overlay="termsOverlay" @close-overlay="termsOverlay = !termsOverlay">
-							<span slot="title"> Service Terms</span>
-							<div slot="content">
-								<v-list class="termsListBox" v-if="form.terms.length > 0">
-									<v-list-item
-										v-for="(term, i) in form.terms"
-										:key="i"
-										no-action
-										class=" termsFont pl-1 elevation-3"
-									>
-										{{ i + 1 }}.<span class="termsItem ml-2">{{ term }} </span>
-									</v-list-item>
-								</v-list>
-								<v-form ref="termsForm" @submit.prevent="termsSubmit" v-model="termsValid">
-									<v-text-field v-model="term">
-										<v-icon slot="append-outer"> fas fa-paper-plane </v-icon>
-									</v-text-field>
-								</v-form>
-							</div>
-							<div class="termsSave d-flex justify-end" slot="buttons">
-								<v-btn
-									color="primary"
-									class="btnCTA "
-									width="110"
-									:ripple="false"
-									@click="termsOverlay = !termsOverlay"
-								>
-									Save
-								</v-btn>
-							</div>
-						</Overlay>
-					</div>
-
-					<!-- Description -->
-					<div class="field">
-						<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
-						<span class="label"> Description:</span>
-						<span class="link" @click="descriptionOverlay = !descriptionOverlay"> Add Description</span>
-						<Overlay
-							:overlay="descriptionOverlay"
-							@close-overlay="descriptionOverlay = !descriptionOverlay"
-						>
-							<span slot="title"> Service Description </span>
-							<div slot="content">
-								<v-form v-model="descriptionValid">
-									<v-textarea
-										color="white"
-										no-resize
-										v-model="form.serviceDescription"
-										height="182px"
-										class="descriptionTextarea pt-0"
-										:rules="descriptionRules"
-										counter="120"
-									>
-									</v-textarea>
-								</v-form>
-							</div>
-							<div class="termsSave d-flex justify-end" slot="buttons">
-								<v-btn
-									color="primary"
-									class="btnCTA"
-									width="110"
-									:ripple="false"
-									@click="descriptionOverlay = !descriptionOverlay"
-								>
-									Save
-								</v-btn>
-							</div>
-						</Overlay>
-					</div>
-
-					<!-- Tags -->
-					<div class="field">
-						<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
-						<span class="label"> Tags:</span>
-
-						<v-combobox
-							clearable
-							multiple
-							small-chips
-							:delimiters="[' ', ',']"
-							v-model="form.tags"
-							class="combobox  pt-0"
-							color="white"
-							:readonly="form.tags.length >= 6"
-							deletable-chips
-							append-icon=""
-						>
-						</v-combobox>
+						</v-checkbox>
 					</div>
 				</div>
-				<v-btn @click="animateForm"> Cancel </v-btn>
-				<v-btn type="submit" :disabled="!serviceValid || !form.terms.length > 0 || !descriptionValid">
-					Submit
-				</v-btn>
+
+				<!-- Software -->
+				<div class="field" :key="7">
+					<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
+					<span class="label"> Software:</span>
+					<v-text-field
+						data-test="software-input"
+						class="pt-0 mt-0 inputText"
+						v-model="form.software"
+						color="#fff"
+						single-line
+						hide-details
+						counter="20"
+						:rules="softwareRules"
+						required
+					>
+					</v-text-field>
+				</div>
+
+				<!-- Terms -->
+				<div class="field" :key="8">
+					<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
+					<span class="label"> Terms:</span>
+					<span class="link align-self-center" @click="termsOverlay = !termsOverlay"> Add Terms</span>
+					<Overlay :overlay="termsOverlay" @close-overlay="termsOverlay = !termsOverlay">
+						<span slot="title"> Service Terms</span>
+						<div slot="content">
+							<v-list class="termsListBox" v-if="form.terms.length > 0">
+								<v-list-item
+									v-for="(term, i) in form.terms"
+									:key="i"
+									no-action
+									class="pl-1 elevation-3 d-flex justify-start "
+								>
+									{{ i + 1 }}.<span class="termsItem ml-2">{{ term }} </span>
+									<v-icon class="deleteTerm" @click="termDelete(i)"> fas fa-times </v-icon>
+								</v-list-item>
+							</v-list>
+							<v-form ref="termsForm" @submit.prevent="termsSubmit" v-model="termsValid">
+								<v-text-field v-model="term">
+									<v-icon slot="append-outer" @click="termsSubmit"> fas fa-paper-plane </v-icon>
+								</v-text-field>
+							</v-form>
+						</div>
+						<div class="termsSave d-flex justify-end" slot="buttons">
+							<v-btn
+								color="primary"
+								class="btnCTA "
+								width="110"
+								:ripple="false"
+								@click="termsOverlay = !termsOverlay"
+							>
+								Save
+							</v-btn>
+						</div>
+					</Overlay>
+				</div>
+
+				<!-- Description -->
+				<div class="field" :key="9">
+					<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
+					<span class="label"> Description:</span>
+					<span class="link align-self-center" @click="descriptionOverlay = !descriptionOverlay">
+						Add Description</span
+					>
+					<Overlay :overlay="descriptionOverlay" @close-overlay="descriptionOverlay = !descriptionOverlay">
+						<span slot="title"> Service Description </span>
+						<div slot="content">
+							<v-form v-model="descriptionValid">
+								<v-textarea
+									color="white"
+									no-resize
+									v-model="form.serviceDescription"
+									height="182px"
+									class="descriptionTextarea pt-0"
+									:rules="descriptionRules"
+									counter="120"
+								>
+								</v-textarea>
+							</v-form>
+						</div>
+						<div class="termsSave d-flex justify-end" slot="buttons">
+							<v-btn
+								color="primary"
+								class="btnCTA"
+								width="110"
+								:ripple="false"
+								@click="descriptionOverlay = !descriptionOverlay"
+							>
+								Save
+							</v-btn>
+						</div>
+					</Overlay>
+				</div>
+
+				<!-- Tags -->
+				<div class="field" :key="10">
+					<Tooltip color="#222222" bgColor="#222222"> save picture </Tooltip>
+					<span class="label"> Tags:</span>
+					<v-combobox
+						clearable
+						multiple
+						small-chips
+						:delimiters="[' ', ',']"
+						v-model="form.tags"
+						class="combobox  pt-0"
+						color="white"
+						:readonly="form.tags.length >= 6"
+						deletable-chips
+						append-icon=""
+					>
+					</v-combobox>
+				</div>
+
+				<!-- Action buttons -->
+				<div class="field justify-end">
+					<v-btn @click="animateForm" class="mr-4"> Cancel </v-btn>
+					<v-btn
+						:loading="createServiceLoading"
+						type="submit"
+						color="primary"
+						:disabled="!serviceValid || !form.terms.length > 0 || !descriptionValid"
+					>
+						Submit
+					</v-btn>
+				</div>
 			</v-form>
 		</div>
 	</div>
@@ -306,30 +314,28 @@ export default {
 	}),
 	mounted() {
 		this.formTween = gsap
-			.timeline()
-			.to(this.$refs.formBox, {
-				height: '760px',
-				duration: 0.3,
-			})
+			.timeline({ onReverseComplete: this.closeForm, onStart: this.openForm })
+			.to(
+				this.$refs.formBox,
+				{
+					height: '720px',
+					duration: 0.3,
+				},
+				0
+			)
+			.from(
+				'.field',
+				{
+					opacity: 0,
+				},
+				0
+			)
 			.pause(0)
 	},
 	methods: {
-		async createService(e) {
-			this.createServiceLoading = true
-			const createService = functions.httpsCallable('createService')
-			await createService({ ...e })
-				.then(() => {
-					this.createServiceLoading = false
-					this.animateForm
-				})
-				.catch((err) => {
-					console.error(err)
-				})
-		},
 		animateForm() {
 			if (this.showForm == false) {
 				this.formTween.play()
-				this.showForm = true
 			} else {
 				this.formTween.reverse()
 				this.form = {
@@ -349,8 +355,13 @@ export default {
 					uid: '',
 					active: true,
 				}
-				this.showForm = false
 			}
+		},
+		closeForm() {
+			this.showForm = false
+		},
+		openForm() {
+			this.showForm = true
 		},
 		processStorePic(event) {
 			this.newStorePic = event
@@ -361,6 +372,9 @@ export default {
 		termsSubmit() {
 			this.form.terms.push(this.term)
 			this.term = ''
+		},
+		termDelete(i) {
+			this.form.terms.splice(i, 1)
 		},
 		async createService() {
 			const hoursToMinutes = this.form.serviceHours * 60
@@ -374,7 +388,7 @@ export default {
 			const createService = functions.httpsCallable('createService')
 			await createService({ ...this.form })
 				.then(() => {
-					this.$refs.serviceForm.reset()
+					this.animateForm()
 					this.createServiceLoading = false
 				})
 				.catch((err) => {
@@ -386,6 +400,19 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.deleteTerm {
+	color: #707070;
+	justify-self: flex-end;
+	margin-left: auto;
+	cursor: pointer;
+	z-index: 1;
+}
+.fieldsContainer {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	height: 100%;
+}
 h1 {
 	font: normal 600 30px/30px Poppins;
 	letter-spacing: -1.5px;
@@ -403,8 +430,8 @@ h1 {
 	align-self: center;
 }
 .field {
+	min-height: 35px;
 	display: flex;
-	margin-top: auto;
 }
 .link {
 	color: #fa4b6b;
@@ -420,6 +447,7 @@ h1 {
 	height: 175px;
 	background-color: #333333;
 	padding: 25px;
+	overflow-x: hidden;
 }
 .newServiceBtn {
 	cursor: pointer;
@@ -468,6 +496,7 @@ h1 {
 ::v-deep .v-color-picker__controls {
 	padding: 0;
 	padding-top: 12px;
+	max-height: 32px;
 }
 ::v-deep .v-color-picker[data-v-c44ffd26] {
 	border: none;
@@ -482,4 +511,10 @@ h1 {
 	padding-bottom: 2px;
 }
 // selection spacing
+@media screen and (max-width: 400px) {
+	.newServiceContainer {
+		padding: 5px;
+		max-width: 375px;
+	}
+}
 </style>
